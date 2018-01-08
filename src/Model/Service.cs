bc57383e887f2476c61059e4cb81c2e9542613af
Dan Schulte <daschult@microsoft.com>
2018-01-08 11:19:43 -0800

    Start adding converted Model types

---
+++ src/Model/Service.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace AutoRest.Java.Model
+{
+    /// <summary>
+    /// A container for the types associated for accessing a specific service.
+    /// </summary>
+    public class Service
+    {
+        private readonly IEnumerable<ServiceEnum> enums;
+        private readonly IEnumerable<ServiceException> exceptions;
+
+        /// <summary>
+        /// Create a new Service with the provided values.
+        /// </summary>
+        /// <param name="enums">The enum types that are used by the service.</param>
+        /// <param name="exceptions">The exception types that are used by the service.</param>
+        public Service(IEnumerable<ServiceEnum> enums, IEnumerable<ServiceException> exceptions)
+        {
+            this.enums = enums;
+            this.exceptions = exceptions;
+        }
+
+        /// <summary>
+        /// Get the enum types that are used by this service.
+        /// </summary>
+        public IEnumerable<ServiceEnum> Enums => enums;
+
+        /// <summary>
+        /// Get the exception types that are used by this service.
+        /// </summary>
+        public IEnumerable<ServiceException> Exceptions => exceptions;
+    }
+}
---
+++ src/Model/ServiceEnum.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace AutoRest.Java.Model
+{
+    /// <summary>
+    /// The details of an enumerated type that is used by a service.
+    /// </summary>
+    public class ServiceEnum
+    {
+        private readonly string name;
+        private readonly string subpackage;
+        private readonly bool expandable;
+        private readonly IEnumerable<ServiceEnumValue> values;
+
+        /// <summary>
+        /// Create a new Enum with the provided properties.
+        /// </summary>
+        /// <param name="name">The name of the new Enum.</param>
+        /// <param name="subpackage">The subpackage that this Enum will appear in.</param>
+        /// <param name="expandable">Whether or not this will be an ExpandableStringEnum type.</param>
+        /// <param name="values">The values of the Enum.</param>
+        public ServiceEnum(string name, string subpackage, bool expandable, IEnumerable<ServiceEnumValue> values)
+        {
+            this.name = name;
+            this.subpackage = subpackage;
+            this.expandable = expandable;
+            this.values = values;
+        }
+
+        /// <summary>
+        /// The name of the new Enum.
+        /// </summary>
+        public string Name => name;
+
+        /// <summary>
+        /// The subpackage that this Enum will appear in.
+        /// </summary>
+        public string Subpackage => subpackage;
+
+        /// <summary>
+        /// Whether or not this will be an ExpandableStringEnum type.
+        /// </summary>
+        public bool Expandable => expandable;
+
+        /// <summary>
+        /// The values of the Enum.
+        /// </summary>
+        public IEnumerable<ServiceEnumValue> Values => values;
+    }
+}
