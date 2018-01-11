981c71c20c56c8cd6829a6946fd16843703424a7
Dan Schulte <daschult@microsoft.com>
2018-01-11 14:12:07 -0800

    Add PageClass

---
+++ src/Model/PageClass.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace AutoRest.Java.Model
+{
+    /// <summary>
+    /// A page class that contains results that are received from a service request.
+    /// </summary>
+    public class PageClass
+    {
+        public PageClass(string nextLinkName, string itemName, string className)
+        {
+            NextLinkName = nextLinkName;
+            ItemName = itemName;
+            ClassName = className;
+        }
+
+        public string NextLinkName { get; }
+
+        public string ItemName { get; }
+
+        public string ClassName { get; }
+    }
+}
