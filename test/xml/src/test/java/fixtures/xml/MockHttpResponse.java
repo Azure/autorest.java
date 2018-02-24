d0538413671a06b4378a60daed34b4c7c6d4f0c4
Rikki Gibson <rigibson@microsoft.com>
2018-02-23 16:10:24 -0800

    Fix null response content bug

--- test/xml/src/test/java/fixtures/xml/MockHttpResponse.java
+++ test/xml/src/test/java/fixtures/xml/MockHttpResponse.java
@@ -45,7 +45,7 @@ public class MockHttpResponse extends HttpResponse {
     }
 
     public MockHttpResponse(int statusCode, HttpHeaders headers) {
-        this(statusCode, headers, null);
+        this(statusCode, headers, new byte[0]);
     }
 
     public MockHttpResponse(int statusCode, HttpHeaders headers, Object serializable) {
