f55a4f798cfc7ad7a9226134d6f453c2f9b85981
Anu Thomas Chandy <anuamd@hotmail.com>
2018-06-21 15:18:03 -0700

    Regenerating autorest tests

--- test/azure/src/main/java/fixtures/lro/implementation/AutoRestLongRunningOperationTestServiceImpl.java
+++ test/azure/src/main/java/fixtures/lro/implementation/AutoRestLongRunningOperationTestServiceImpl.java
@@ -35,11 +35,11 @@ public class AutoRestLongRunningOperationTestServiceImpl extends AzureServiceCli
         return this.azureClient;
     }
 
-    /** Gets or sets the preferred language for the response. */
+    /** The preferred language for the response. */
     private String acceptLanguage;
 
     /**
-     * Gets Gets or sets the preferred language for the response.
+     * Gets The preferred language for the response.
      *
      * @return the acceptLanguage value.
      */
@@ -48,7 +48,7 @@ public class AutoRestLongRunningOperationTestServiceImpl extends AzureServiceCli
     }
 
     /**
-     * Sets Gets or sets the preferred language for the response.
+     * Sets The preferred language for the response.
      *
      * @param acceptLanguage the acceptLanguage value.
      * @return the service client itself
@@ -58,11 +58,11 @@ public class AutoRestLongRunningOperationTestServiceImpl extends AzureServiceCli
         return this;
     }
 
-    /** Gets or sets the retry timeout in seconds for Long Running Operations. Default value is 30. */
+    /** The retry timeout in seconds for Long Running Operations. Default value is 30. */
     private int longRunningOperationRetryTimeout;
 
     /**
-     * Gets Gets or sets the retry timeout in seconds for Long Running Operations. Default value is 30.
+     * Gets The retry timeout in seconds for Long Running Operations. Default value is 30.
      *
      * @return the longRunningOperationRetryTimeout value.
      */
@@ -71,7 +71,7 @@ public class AutoRestLongRunningOperationTestServiceImpl extends AzureServiceCli
     }
 
     /**
-     * Sets Gets or sets the retry timeout in seconds for Long Running Operations. Default value is 30.
+     * Sets The retry timeout in seconds for Long Running Operations. Default value is 30.
      *
      * @param longRunningOperationRetryTimeout the longRunningOperationRetryTimeout value.
      * @return the service client itself
@@ -81,11 +81,11 @@ public class AutoRestLongRunningOperationTestServiceImpl extends AzureServiceCli
         return this;
     }
 
-    /** When set to true a unique x-ms-client-request-id value is generated and included in each request. Default is true. */
+    /** Whether a unique x-ms-client-request-id should be generated. When set to true a unique x-ms-client-request-id value is generated and included in each request. Default is true. */
     private boolean generateClientRequestId;
 
     /**
-     * Gets When set to true a unique x-ms-client-request-id value is generated and included in each request. Default is true.
+     * Gets Whether a unique x-ms-client-request-id should be generated. When set to true a unique x-ms-client-request-id value is generated and included in each request. Default is true.
      *
      * @return the generateClientRequestId value.
      */
@@ -94,7 +94,7 @@ public class AutoRestLongRunningOperationTestServiceImpl extends AzureServiceCli
     }
 
     /**
-     * Sets When set to true a unique x-ms-client-request-id value is generated and included in each request. Default is true.
+     * Sets Whether a unique x-ms-client-request-id should be generated. When set to true a unique x-ms-client-request-id value is generated and included in each request. Default is true.
      *
      * @param generateClientRequestId the generateClientRequestId value.
      * @return the service client itself
