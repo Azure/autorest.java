3b90c12d4997b68aa6d9b0cea72b72a25f2484ca
Weidong Xu <weidxu@microsoft.com>
2019-11-26 02:39:12 +0800

    update fluent pom template  (#403)
    * update fluent pom template due to folder restructuring in azure-sdk-for-java
    
    * azure-sdk-for-java adjust folder structure again. no need to change relativePath now

--- src/azurefluent/Model/CodeModelJvaf.cs
+++ src/azurefluent/Model/CodeModelJvaf.cs
@@ -116,7 +116,7 @@ namespace AutoRest.Java.Azure.Fluent.Model
             }
         }
 
-        private const string autogenedLibParentPomVersion = "1.1.0";
+        private const string autogenedLibParentPomVersion = "1.2.0";
         private const string fluentPremiumLibParentPomVersion = "1.22.0";
 
         [JsonIgnore]
