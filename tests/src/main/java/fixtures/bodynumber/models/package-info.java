e578ea9914642a77046e4103adca15cc51044d7e
Srikanta <srnagar@microsoft.com>
2019-12-06 15:34:14 -0800

    Generate code for body-number

---
+++ tests/src/main/java/fixtures/bodynumber/models/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containing the data models for AutoRestNumberTestService.
+ * Test Infrastructure for AutoRest.
+ */
+package fixtures.bodynumber.models;
---
+++ tests/src/main/java/fixtures/bodynumber/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containing the classes for AutoRestNumberTestService.
+ * Test Infrastructure for AutoRest.
+ */
+package fixtures.bodynumber;
