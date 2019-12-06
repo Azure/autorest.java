ad6893f4af1388f5324e76e77edb2cd2b42de3c4
Weidong Xu <weidxu@microsoft.com>
2019-12-06 12:29:20 +0800

    update generate

--- generate.bat
+++ generate.bat
@@ -3,6 +3,8 @@ call autorest --java --use:.\ --input-file=https://raw.githubusercontent.com/Azu
 call autorest --java --use:.\ --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/body-file.json --namespace=fixtures.bodyfile --output-folder=tests --sync-methods=all
 call autorest --java --use:.\ --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/body-string.json --namespace=fixtures.bodystring --output-folder=tests --sync-methods=all
 call autorest --java --use:.\ --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/custom-baseUrl.json --namespace=fixtures.custombaseuri --output-folder=tests --sync-methods=all
+call autorest --java --use:.\ --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/head.json --namespace=fixtures.head --output-folder=tests --sync-methods=all
+call autorest --java --use:.\ --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/head-exceptions.json --namespace=fixtures.headexceptions --output-folder=tests --sync-methods=all
 call autorest --java --use:.\ --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/header.json --namespace=fixtures.header --output-folder=tests --sync-methods=all
 call autorest --java --use:.\ --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/body-dictionary.json --namespace=fixtures.bodydictionary --output-folder=tests --sync-methods=all
 
