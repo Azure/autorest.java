e2eee0c398b627f4273c4436bcdd647c1ddfa5a8
Weidong Xu <weidxu@microsoft.com>
2019-12-12 09:54:51 +0800

    codegen and tests for body-byte

---
+++ tests/src/main/java/fixtures/bodybyte/AutoRestSwaggerBATByteServiceBuilder.java
@@ -0,0 +1,65 @@
+package fixtures.bodybyte;
+
+import com.azure.core.annotation.ServiceClientBuilder;
+import com.azure.core.http.HttpPipeline;
+import com.azure.core.http.HttpPipelineBuilder;
+import com.azure.core.http.policy.CookiePolicy;
+import com.azure.core.http.policy.RetryPolicy;
+import com.azure.core.http.policy.UserAgentPolicy;
+
+/**
+ * A builder for creating a new instance of the AutoRestSwaggerBATByteService type.
+ */
+@ServiceClientBuilder(serviceClients = AutoRestSwaggerBATByteService.class)
+public final class AutoRestSwaggerBATByteServiceBuilder {
+    /*
+     * http://localhost:3000
+     */
+    private String host;
+
+    /**
+     * Sets http://localhost:3000.
+     * 
+     * @param host the host value.
+     * @return the AutoRestSwaggerBATByteServiceBuilder.
+     */
+    public AutoRestSwaggerBATByteServiceBuilder host(String host) {
+        this.host = host;
+        return this;
+    }
+
+    /*
+     * The HTTP pipeline to send requests through
+     */
+    private HttpPipeline pipeline;
+
+    /**
+     * Sets The HTTP pipeline to send requests through.
+     * 
+     * @param pipeline the pipeline value.
+     * @return the AutoRestSwaggerBATByteServiceBuilder.
+     */
+    public AutoRestSwaggerBATByteServiceBuilder pipeline(HttpPipeline pipeline) {
+        this.pipeline = pipeline;
+        return this;
+    }
+
+    /**
+     * Builds an instance of AutoRestSwaggerBATByteService with the provided parameters.
+     * 
+     * @return an instance of AutoRestSwaggerBATByteService.
+     */
+    public AutoRestSwaggerBATByteService build() {
+        if (host == null) {
+            this.host = "http://localhost:3000";
+        }
+        if (pipeline == null) {
+            this.pipeline = new HttpPipelineBuilder().policies(new UserAgentPolicy(), new RetryPolicy(), new CookiePolicy()).build();
+        }
+        AutoRestSwaggerBATByteService client = new AutoRestSwaggerBATByteService(pipeline);
+        if (this.host != null) {
+            client.setHost(this.host);
+        }
+        return client;
+    }
+}
