405e54a779e6dc583075655fddba3f5e9a0616c6
Weidong Xu <weidxu@microsoft.com>
2019-12-13 13:55:07 +0800

    nic

--- javagen/src/main/java/com/azure/autorest/mapper/ServiceClientMapper.java
+++ javagen/src/main/java/com/azure/autorest/mapper/ServiceClientMapper.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ServiceClientMapper implements IMapper<CodeModel, ServiceClient> {
