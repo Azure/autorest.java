c825d6d17e58360cf1e4859f9145a7352af2be21
Jianghao Lu <jianghaolu@users.noreply.github.com>
2019-12-13 15:59:15 -0800

    Generate extensible-enums-swagger.json (#462)
    * Generate extensible-enums-swagger.json
    
    * Modify generate script

---
+++ tests/src/main/java/fixtures/extensibleenums/models/Pet.java
@@ -0,0 +1,88 @@
+package fixtures.extensibleenums.models;
+
+import com.azure.core.annotation.Fluent;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * The Pet model.
+ */
+@Fluent
+public final class Pet {
+    /*
+     * The name property.
+     */
+    @JsonProperty(value = "name")
+    private String name;
+
+    /*
+     * The DaysOfWeek property.
+     */
+    @JsonProperty(value = "DaysOfWeek")
+    private DaysOfWeekExtensibleEnum daysOfWeek;
+
+    /*
+     * The IntEnum property.
+     */
+    @JsonProperty(value = "IntEnum", required = true)
+    private IntEnum intEnum;
+
+    /**
+     * Get the name property: The name property.
+     * 
+     * @return the name value.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Set the name property: The name property.
+     * 
+     * @param name the name value to set.
+     * @return the Pet object itself.
+     */
+    public Pet setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    /**
+     * Get the daysOfWeek property: The DaysOfWeek property.
+     * 
+     * @return the daysOfWeek value.
+     */
+    public DaysOfWeekExtensibleEnum getDaysOfWeek() {
+        return this.daysOfWeek;
+    }
+
+    /**
+     * Set the daysOfWeek property: The DaysOfWeek property.
+     * 
+     * @param daysOfWeek the daysOfWeek value to set.
+     * @return the Pet object itself.
+     */
+    public Pet setDaysOfWeek(DaysOfWeekExtensibleEnum daysOfWeek) {
+        this.daysOfWeek = daysOfWeek;
+        return this;
+    }
+
+    /**
+     * Get the intEnum property: The IntEnum property.
+     * 
+     * @return the intEnum value.
+     */
+    public IntEnum getIntEnum() {
+        return this.intEnum;
+    }
+
+    /**
+     * Set the intEnum property: The IntEnum property.
+     * 
+     * @param intEnum the intEnum value to set.
+     * @return the Pet object itself.
+     */
+    public Pet setIntEnum(IntEnum intEnum) {
+        this.intEnum = intEnum;
+        return this;
+    }
+}
---
+++ tests/src/test/java/fixtures/extensibleenums/PetsTests.java
@@ -0,0 +1,47 @@
+package fixtures.extensibleenums;
+
+import fixtures.extensibleenums.models.DaysOfWeekExtensibleEnum;
+import fixtures.extensibleenums.models.IntEnum;
+import fixtures.extensibleenums.models.Pet;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PetsTests {
+    private static PetStoreInc client;
+
+    @BeforeClass
+    public static void setup() {
+        client = new PetStoreIncBuilder().build();
+    }
+
+    @Test
+    public void getByPetId() throws Exception {
+        Pet tommy = client.pets().getByPetId("tommy");
+        Assert.assertNotNull(tommy);
+        Assert.assertEquals(DaysOfWeekExtensibleEnum.MONDAY, tommy.getDaysOfWeek());
+        Assert.assertEquals(IntEnum.ONE, tommy.getIntEnum());
+        Assert.assertEquals("Tommy Tomson", tommy.getName());
+
+        Pet casper = client.pets().getByPetId("casper");
+        Assert.assertNotNull(casper);
+        Assert.assertEquals(DaysOfWeekExtensibleEnum.fromString("Weekend"), casper.getDaysOfWeek());
+        Assert.assertEquals(IntEnum.TWO, casper.getIntEnum());
+        Assert.assertEquals("Casper Ghosty", casper.getName());
+
+        Pet scooby = client.pets().getByPetId("scooby");
+        Assert.assertNotNull(scooby);
+        Assert.assertEquals(DaysOfWeekExtensibleEnum.THURSDAY, scooby.getDaysOfWeek());
+        Assert.assertEquals(IntEnum.fromString("2.1"), scooby.getIntEnum());
+        Assert.assertEquals("Scooby Scarface", scooby.getName());
+    }
+
+    @Test
+    public void addPet() throws Exception {
+        Pet pet = new Pet().setName("Retriever");
+        Pet res = client.pets().addPet(pet);
+        Assert.assertEquals(pet.getName(), res.getName());
+        Assert.assertNull(res.getDaysOfWeek());
+        Assert.assertNull(res.getIntEnum());
+    }
+}
