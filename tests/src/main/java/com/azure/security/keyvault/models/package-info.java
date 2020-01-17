0bb9d18b12aa71cf1ecb11b6d4c584efff6ece5b
Srikanta <srnagar@microsoft.com>
2020-01-17 15:20:15 -0800

    Remove KV changes

--- tests/src/main/java/com/azure/security/keyvault/models/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Package containing the data models for KeyVaultClient.
- * The key vault client performs cryptographic key operations and vault
- * operations against the Key Vault service.
- */
-package com.azure.security.keyvault.models;
--- tests/src/main/java/com/azure/security/keyvault/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Package containing the classes for KeyVaultClient.
- * The key vault client performs cryptographic key operations and vault
- * operations against the Key Vault service.
- */
-package com.azure.security.keyvault;
