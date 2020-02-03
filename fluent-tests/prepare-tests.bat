a85d27f75ae54bc41bf5b4d6e1b537fe4540bacf
Weidong Xu <weidxu@microsoft.com>
2020-02-03 22:10:23 +0800

    add test

--- fluent-tests/prepare-tests.bat
+++ fluent-tests/prepare-tests.bat
@@ -6,3 +6,5 @@ SET COMMON_ARGUMENTS=--java --use:../fluentgen --output-folder=./ --sync-methods
 CALL autorest-beta --version=%AUTOREST_CORE_VERSION% %COMMON_ARGUMENTS% --input-file=https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/resources/resource-manager/Microsoft.Resources/stable/2019-08-01/resources.json --namespace=com.azure.mgmttest.resources
 
 CALL autorest-beta --version=%AUTOREST_CORE_VERSION% %COMMON_ARGUMENTS% --payload-flattening-threshold=2 ./swagger/readme.storage.md --namespace=com.azure.mgmttest.storage
+
+CALL autorest-beta --version=%AUTOREST_CORE_VERSION% %COMMON_ARGUMENTS% --payload-flattening-threshold=1 ./swagger/readme.network.md --namespace=com.azure.mgmttest.network
