f6e52770aeb839e94b927368b2dc6dd69586ca12
Weidong Xu <weidxu@microsoft.com>
2020-02-03 17:46:37 +0800

    minor change

--- javagen/src/main/java/com/azure/autorest/util/DefaultNamerFactory.java
+++ javagen/src/main/java/com/azure/autorest/util/DefaultNamerFactory.java
@@ -8,7 +8,7 @@ package com.azure.autorest.util;
 
 public class DefaultNamerFactory implements NamerFactory {
 
-    protected static final ModelNamer modelNamer = new ModelNamer();
+    private static final ModelNamer modelNamer = new ModelNamer();
 
     @Override
     public ModelNamer getModelNamer() {
