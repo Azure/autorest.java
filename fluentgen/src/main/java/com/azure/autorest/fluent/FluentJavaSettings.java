fd1c01e76ecafae9581571587155ba58c8ea08ee
Weidong Xu <weidxu@microsoft.com>
2020-02-03 09:38:32 +0800

    cleanup code no longer in use

--- fluentgen/src/main/java/com/azure/autorest/fluent/FluentJavaSettings.java
+++ fluentgen/src/main/java/com/azure/autorest/fluent/FluentJavaSettings.java
@@ -20,40 +20,6 @@ public class FluentJavaSettings {
 
     private Set<String> javaNamesForAddInner;
 
-    private Set<ClientFlattenProperty> javaNamesForClientFlatten;
-
-    public static class ClientFlattenProperty {
-        private final String javaName;
-        private final String propertySerializedName;
-
-        public ClientFlattenProperty(String javaName, String propertySerializedName) {
-            this.javaName = javaName;
-            this.propertySerializedName = propertySerializedName;
-        }
-
-        public String getJavaName() {
-            return javaName;
-        }
-
-        public String getPropertySerializedName() {
-            return propertySerializedName;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            ClientFlattenProperty that = (ClientFlattenProperty) o;
-            return javaName.equals(that.javaName) &&
-                    propertySerializedName.equals(that.propertySerializedName);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(javaName, propertySerializedName);
-        }
-    }
-
     public FluentJavaSettings(NewPlugin host) {
         Objects.requireNonNull(host);
         this.host = host;
@@ -65,10 +31,6 @@ public class FluentJavaSettings {
         return javaNamesForAddInner;
     }
 
-    public Set<ClientFlattenProperty> getJavaNamesForClientFlatten() {
-        return javaNamesForClientFlatten;
-    }
-
     private void loadSettings() {
         String addInnerSetting = host.getStringValue("add-inner");
         if (addInnerSetting != null && !addInnerSetting.isEmpty()) {
@@ -78,24 +40,6 @@ public class FluentJavaSettings {
                     .collect(Collectors.toSet());
         } else {
             javaNamesForAddInner = Collections.emptySet();
-        }
-
-        String addClientFlattenSetting = host.getStringValue("add-client-flatten");
-        if (addClientFlattenSetting != null && !addClientFlattenSetting.isEmpty()) {
-            javaNamesForClientFlatten = Arrays.stream(addClientFlattenSetting.split(Pattern.quote(",")))
-                    .map(String::trim)
-                    .filter(s -> !s.isEmpty())
-                    .map(s -> {
-                        String[] segs = s.split(Pattern.quote("."));
-                        if (segs.length == 1) {
-                            return new ClientFlattenProperty(segs[0], "properties");
-                        } else if (segs.length == 2) {
-                            return new ClientFlattenProperty(segs[0], segs[1]);
-                        } else {
-                            throw new IllegalArgumentException("Failed to parse client flatten: " + s);
-                        }
-                    })
-                    .collect(Collectors.toSet());
         }
     }
 }
--- fluentgen/src/main/java/com/azure/autorest/fluent/template/FluentTemplateFactory.java
+++ fluentgen/src/main/java/com/azure/autorest/fluent/template/FluentTemplateFactory.java
@@ -5,13 +5,8 @@
 
 package com.azure.autorest.fluent.template;
 
-import com.azure.autorest.template.ClientMethodTemplate;
 import com.azure.autorest.template.DefaultTemplateFactory;
 
 public class FluentTemplateFactory extends DefaultTemplateFactory {
 
-//    @Override
-//    public ClientMethodTemplate getClientMethodTemplate() {
-//        return FluentClientMethodTemplate.getInstance();
-//    }
 }
