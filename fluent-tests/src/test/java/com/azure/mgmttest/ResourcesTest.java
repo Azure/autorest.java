43b546c9485cb2f42ae5734e41d388cb2ee74895
Weidong Xu <weidxu@microsoft.com>
2020-02-05 09:28:49 +0800

    enhance test

--- fluent-tests/src/test/java/com/azure/mgmttest/ResourcesTest.java
+++ fluent-tests/src/test/java/com/azure/mgmttest/ResourcesTest.java
@@ -6,12 +6,44 @@
 
 package com.azure.mgmttest;
 
-import org.junit.Test;
+import com.azure.core.management.Resource;
+import com.azure.management.resources.fluentcore.collection.InnerSupportsGet;
+import com.azure.management.resources.fluentcore.collection.InnerSupportsListing;
+import com.azure.mgmttest.network.models.NetworkInterfacesInner;
+import com.azure.mgmttest.network.models.NetworkSecurityGroupInner;
+import com.azure.mgmttest.resources.models.DeploymentExtendedInner;
+import com.azure.mgmttest.resources.models.DeploymentsInner;
+import com.azure.mgmttest.resources.models.ResourceGroupInner;
+import com.azure.mgmttest.storage.models.StorageAccountInner;
+import com.azure.mgmttest.storage.models.StorageAccountsInner;
+
+import static org.mockito.Mockito.*;
 
 public class ResourcesTest {
 
-    @Test
-    public void test() {
+    // Verify method signature at compile time.
 
+    public void testOperationName() {
+        NetworkInterfacesInner nic = mock(NetworkInterfacesInner.class);
+        nic.list();
+        nic.listAsync();
+        nic.listByResourceGroup(anyString());
+        nic.listByResourceGroupAsync(anyString());
+    }
+
+    public void testInnerSupport() {
+        InnerSupportsListing<StorageAccountInner> sa = mock(StorageAccountsInner.class);
+        sa.list();
+
+        InnerSupportsGet<DeploymentExtendedInner> de = mock(DeploymentsInner.class);
+        de.getByResourceGroup(anyString(), anyString());
+    }
+
+    public void testResourceType() {
+        Resource rg = mock(ResourceGroupInner.class);
+        rg.getId();
+
+        NetworkSecurityGroupInner nsg = mock(NetworkSecurityGroupInner.class);
+        nsg.withId(anyString());
     }
 }
