bae88b07823e487fc0dd53b4c3f812dff1501c60
Weidong Xu <weidxu@microsoft.com>
2020-03-05 16:33:42 +0800

    avoid conflict with HeaderParam when adding default headers

--- fluentgen/src/main/java/com/azure/autorest/fluent/template/FluentProxyTemplate.java
+++ fluentgen/src/main/java/com/azure/autorest/fluent/template/FluentProxyTemplate.java
@@ -6,9 +6,16 @@
 
 package com.azure.autorest.fluent.template;
 
+import com.azure.autorest.extension.base.model.codemodel.RequestParameterLocation;
 import com.azure.autorest.model.clientmodel.ProxyMethod;
+import com.azure.autorest.model.clientmodel.ProxyMethodParameter;
 import com.azure.autorest.model.javamodel.JavaInterface;
 import com.azure.autorest.template.ProxyTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FluentProxyTemplate extends ProxyTemplate {
 
@@ -20,8 +27,26 @@ public class FluentProxyTemplate extends ProxyTemplate {
 
     @Override
     protected void writeProxyMethodHeaders(ProxyMethod restAPIMethod, JavaInterface interfaceBlock) {
-        interfaceBlock.annotation(String.format("Headers({ \"Content-Type: %s\", \"Accept: %s\" })",
-                restAPIMethod.getRequestContentType(),
-                String.join(",", restAPIMethod.getResponseContentTypes())));
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", restAPIMethod.getRequestContentType());
+        headers.put("Accept", String.join(",", restAPIMethod.getResponseContentTypes()));
+
+        Set<String> headerParameterNames = restAPIMethod.getParameters().stream()
+                .filter(p -> p.getRequestParameterLocation() == RequestParameterLocation.Header)
+                .map(ProxyMethodParameter::getRequestParameterName)
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
+
+        headers = headers.entrySet().stream()
+                .filter(e -> !headerParameterNames.contains(e.getKey().toLowerCase()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        if (!headers.isEmpty()) {
+            String headersString = headers.entrySet().stream()
+                    .map(e -> String.format("\"%s: %s\"", e.getKey(), e.getValue()))
+                    .collect(Collectors.joining(", "));
+            interfaceBlock.annotation(String.format("Headers({ %s })",
+                    headersString));
+        }
     }
 }
