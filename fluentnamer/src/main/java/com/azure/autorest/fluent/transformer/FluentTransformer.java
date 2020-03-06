3dde8d631b0506a7905d70dc9177cb23808a5a17
Weidong Xu <weidxu@microsoft.com>
2020-03-06 18:27:12 +0800

    remove tranformer

--- fluentnamer/src/main/java/com/azure/autorest/fluent/transformer/FluentTransformer.java
+++ fluentnamer/src/main/java/com/azure/autorest/fluent/transformer/FluentTransformer.java
@@ -53,7 +53,6 @@ public class FluentTransformer {
 
     public CodeModel postTransform(CodeModel codeModel) {
         codeModel = new FlattenedTypeCleanup().process(codeModel);
-        //codeModel = new MultipleInheritanceFlatten().process(codeModel);
         codeModel = new OperationNameNormalization().process(codeModel);
         codeModel = new ResourceTypeNormalization().process(codeModel);
         if (fluentJavaSettings.isResourcePropertyAsSubResource()) {
