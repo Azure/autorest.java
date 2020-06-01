771ff79b5fca9605ad16632e1666ce38ada79c3a
Weidong Xu <weidxu@microsoft.com>
2020-06-01 09:32:44 +0800

    single place to get builder suffix (#619)

--- javagen/src/main/java/com/azure/autorest/template/ServiceAsyncClientTemplate.java
+++ javagen/src/main/java/com/azure/autorest/template/ServiceAsyncClientTemplate.java
@@ -6,6 +6,8 @@ import com.azure.autorest.model.clientmodel.ClientMethodType;
 import com.azure.autorest.model.clientmodel.MethodGroupClient;
 import com.azure.autorest.model.clientmodel.ServiceClient;
 import com.azure.autorest.model.javamodel.JavaFile;
+import com.azure.autorest.util.ClientModelUtil;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -46,7 +48,8 @@ public class ServiceAsyncClientTemplate implements IJavaTemplate<AsyncSyncClient
         comment.description(String.format("Initializes a new instance of the asynchronous %1$s type.",
             serviceClient.getInterfaceName())));
 
-    javaFile.annotation(String.format("ServiceClient(builder = %sBuilder.class)", serviceClient.getClientBaseName()));
+    javaFile.annotation(String.format("ServiceClient(builder = %s.class)",
+            serviceClient.getInterfaceName() + ClientModelUtil.getBuilderSuffix()));
     javaFile.publicFinalClass(asyncClassName, classBlock ->
     {
       // Add service client member variable
