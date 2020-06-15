b015db4eac0a310eee8dcc584d24262492fd29e3
Weidong Xu <weidxu@microsoft.com>
2020-06-15 16:28:49 +0800

    support remove-inner

--- fluentgen/src/main/java/com/azure/autorest/fluent/mapper/FluentObjectMapper.java
+++ fluentgen/src/main/java/com/azure/autorest/fluent/mapper/FluentObjectMapper.java
@@ -7,6 +7,7 @@ package com.azure.autorest.fluent.mapper;
 
 import com.azure.autorest.extension.base.model.codemodel.ObjectSchema;
 import com.azure.autorest.fluent.model.FluentType;
+import com.azure.autorest.fluent.util.Utils;
 import com.azure.autorest.mapper.ObjectMapper;
 import com.azure.autorest.model.clientmodel.ClassType;
 import com.azure.core.util.FluxUtil;
@@ -15,6 +16,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class FluentObjectMapper extends ObjectMapper {
 
@@ -53,6 +55,7 @@ public class FluentObjectMapper extends ObjectMapper {
 
     /**
      * Add types as Inner.
+     *
      * @param compositeTypes The types to add as Inner.
      * @return The types from compositeTypes that need to be added.
      */
@@ -61,5 +64,17 @@ public class FluentObjectMapper extends ObjectMapper {
         compositeTypesToAdd.removeAll(innerModels);
         innerModels.addAll(compositeTypesToAdd);
         return compositeTypesToAdd;
+    }
+
+    /**
+     * Remove types as Inner.
+     *
+     * @param javaNames The Java class names to remove as Inner.
+     */
+    public void removeInnerModels(Set<String> javaNames) {
+        Set<ObjectSchema> compositeTypesToRemove = innerModels.stream()
+                .filter(type -> javaNames.contains(Utils.getJavaName(type)))
+                .collect(Collectors.toSet());
+        innerModels.removeAll(compositeTypesToRemove);
     }
 }
