4c51877c57cc573c8c264a2393a6de760f23006b
Weidong Xu <weidxu@microsoft.com>
2020-08-26 11:20:22 +0800

    move client method with only required parameter before WithResponse + Context

--- javagen/src/main/java/com/azure/autorest/mapper/ClientMethodMapper.java
+++ javagen/src/main/java/com/azure/autorest/mapper/ClientMethodMapper.java
@@ -425,17 +425,18 @@ public class ClientMethodMapper implements IMapper<Operation, List<ClientMethod>
                             .isGroupedParameterRequired(false)
                             .build());
 
-                    if (settings.isContextClientMethodParameter()) {
-                        builder.name(proxyMethod.getSimpleRestResponseMethodName()).returnValue(new ReturnValue(
-                                returnTypeDescription(operation, syncReturnWithResponse, syncReturnWithResponse),
-                                syncReturnWithResponse));
-                        addClientMethodWithContext(methods, builder, parameters);
-                    }
-
                     if (generateClientMethodWithOnlyRequiredParameters) {
                         methods.add(builder
                                 .onlyRequiredParameters(true)
                                 .build());
+                    }
+
+                    if (settings.isContextClientMethodParameter()) {
+                        builder.onlyRequiredParameters(false)
+                                .name(proxyMethod.getSimpleRestResponseMethodName())
+                                .returnValue(new ReturnValue(returnTypeDescription(operation, syncReturnWithResponse,
+                                        syncReturnWithResponse), syncReturnWithResponse));
+                        addClientMethodWithContext(methods, builder, parameters);
                     }
                 }
             }
