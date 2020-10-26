9aacb026fff608f6fe1c294a6774740f15e78e3b
Weidong Xu <weidxu@microsoft.com>
2020-10-26 23:00:20 +0800

    clean up unused code

--- javagen/src/main/java/com/azure/autorest/model/clientmodel/ClientModels.java
+++ javagen/src/main/java/com/azure/autorest/model/clientmodel/ClientModels.java
@@ -12,40 +12,39 @@ import java.util.Map;
 /**
  * The collection of all client models stored for inheritance lookup.
  */
-//C# TO JAVA CONVERTER TODO TASK: The interface type was changed to the closest equivalent Java type, but the methods implemented will need adjustment:
 public class ClientModels {
     public static final ClientModels Instance = new ClientModels();
-    private final Map<String, ClientModel> nameMap = new HashMap<String, ClientModel>();
-    private final Map<String, ArrayList<ClientModel>> derivedTypesMap = new HashMap<String, ArrayList<ClientModel>>();
+    private final Map<String, ClientModel> nameMap = new HashMap<>();
+//    private final Map<String, ArrayList<ClientModel>> derivedTypesMap = new HashMap<String, ArrayList<ClientModel>>();
     private ClientModels() {
     }
 
     public final ClientModel getModel(String modelName) {
-        return nameMap.containsKey(modelName) ? nameMap.get(modelName) : null;
+        return nameMap.get(modelName);
     }
 
     public final void addModel(ClientModel model) {
         nameMap.put(model.getName(), model);
 
-        String parentModel = model.getParentModelName();
-        if (parentModel != null) {
-            ArrayList<ClientModel> derivedTypesList = getDerivedTypeList(parentModel);
-            derivedTypesList.add(model);
-        }
+//        String parentModel = model.getParentModelName();
+//        if (parentModel != null) {
+//            ArrayList<ClientModel> derivedTypesList = getDerivedTypeList(parentModel);
+//            derivedTypesList.add(model);
+//        }
     }
 
-    public final List<ClientModel> getDerivedTypes(String parentModelName) {
-        return getDerivedTypeList(parentModelName);
-    }
+//    public final List<ClientModel> getDerivedTypes(String parentModelName) {
+//        return getDerivedTypeList(parentModelName);
+//    }
 
     public final List<ClientModel> getTypes() {
         return new ArrayList<>(nameMap.values());
     }
 
-    private ArrayList<ClientModel> getDerivedTypeList(String parentModelName) {
-        if (!derivedTypesMap.containsKey(parentModelName)) {
-            derivedTypesMap.put(parentModelName, new ArrayList<ClientModel>());
-        }
-        return derivedTypesMap.get(parentModelName);
-    }
+//    private ArrayList<ClientModel> getDerivedTypeList(String parentModelName) {
+//        if (!derivedTypesMap.containsKey(parentModelName)) {
+//            derivedTypesMap.put(parentModelName, new ArrayList<ClientModel>());
+//        }
+//        return derivedTypesMap.get(parentModelName);
+//    }
 }
