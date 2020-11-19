b4b5af587b0c66f025616167f23f4f0f2f7d0ae5
JP Chen <jiach@microsoft.com>
2020-11-19 09:31:40 -0800

    Fix import *

--- androidgen/src/main/java/com/azure/autorest/android/template/AndroidEmbeddedBuilderTemplate.java
+++ androidgen/src/main/java/com/azure/autorest/android/template/AndroidEmbeddedBuilderTemplate.java
@@ -4,7 +4,12 @@
 package com.azure.autorest.android.template;
 
 import com.azure.autorest.extension.base.plugin.JavaSettings;
-import com.azure.autorest.model.clientmodel.*;
+import com.azure.autorest.model.clientmodel.AsyncSyncClient;
+import com.azure.autorest.model.clientmodel.ClassType;
+import com.azure.autorest.model.clientmodel.ClientMethodParameter;
+import com.azure.autorest.model.clientmodel.Constructor;
+import com.azure.autorest.model.clientmodel.IType;
+import com.azure.autorest.model.clientmodel.ServiceClient;
 import com.azure.autorest.model.javamodel.JavaBlock;
 import com.azure.autorest.model.javamodel.JavaClass;
 import com.azure.autorest.model.javamodel.JavaVisibility;
