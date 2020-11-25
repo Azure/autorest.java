31ad6105f15755314f3c93125b8d5231105aac81
Weidong Xu <weidxu@microsoft.com>
2020-11-25 21:24:35 +0800

    check name conflict as case insensitive (#815)

--- fluentgen/src/main/java/com/azure/autorest/fluent/template/FluentProxyTemplate.java
+++ fluentgen/src/main/java/com/azure/autorest/fluent/template/FluentProxyTemplate.java
@@ -13,6 +13,7 @@ import com.azure.autorest.model.javamodel.JavaInterface;
 import com.azure.autorest.template.ProxyTemplate;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -38,7 +39,7 @@ public class FluentProxyTemplate extends ProxyTemplate {
                 .collect(Collectors.toSet());
 
         headers = headers.entrySet().stream()
-                .filter(e -> !headerParameterNames.contains(e.getKey().toLowerCase()))
+                .filter(e -> !headerParameterNames.contains(e.getKey().toLowerCase(Locale.ROOT)))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
         if (!headers.isEmpty()) {
