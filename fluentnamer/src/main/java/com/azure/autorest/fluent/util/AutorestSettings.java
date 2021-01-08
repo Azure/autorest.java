28e93d45eaa2e2cc87cdda7207ac36545c76f7bb
Weidong Xu <weidxu@microsoft.com>
2021-01-08 12:26:21 +0800

    v4 fluentlite load input-file from autorest (#878)

---
+++ fluentnamer/src/main/java/com/azure/autorest/fluent/util/AutorestSettings.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.azure.autorest.fluent.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class AutorestSettings {
+
+    private String tag;
+    private String baseFolder;
+    private String outputFolder;
+    private String azureLibrariesForJavaFolder;
+    private final List<String> inputFiles = new ArrayList<>();
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setBaseFolder(String baseFolder) {
+        this.baseFolder = baseFolder;
+    }
+
+    public void setOutputFolder(String outputFolder) {
+        this.outputFolder = outputFolder;
+    }
+
+    public void setAzureLibrariesForJavaFolder(String azureLibrariesForJavaFolder) {
+        this.azureLibrariesForJavaFolder = azureLibrariesForJavaFolder;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getBaseFolder() {
+        return baseFolder;
+    }
+
+    public String getOutputFolder() {
+        return outputFolder;
+    }
+
+    public Optional<String> getAzureLibrariesForJavaFolder() {
+        return Optional.ofNullable(azureLibrariesForJavaFolder);
+    }
+
+    public List<String> getInputFiles() {
+        return inputFiles;
+    }
+}
--- generate.bat
+++ generate.bat
@@ -1,6 +1,6 @@
 set VANILLA_ARGUMENTS=--version=3.0.6350 --java --use=. --output-folder=vanilla-tests --sync-methods=all --client-side-validations --add-context-parameter --required-parameter-client-methods
 set AZURE_ARGUMENTS=--version=3.0.6350 --java --use=. --output-folder=azure-tests --sync-methods=all --client-side-validations --add-context-parameter --required-parameter-client-methods
-set ARM_ARGUMENTS=--version=3.0.6350 --java --use=. --output-folder=azure-tests --azure-arm --fluent=lite --pom-file=pom_arm.xml
+set ARM_ARGUMENTS=--version=3.0.6350 --java --use=. --output-folder=azure-tests --azure-arm --fluent=lite --regenerate-pom=false
 
 call autorest %VANILLA_ARGUMENTS% --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/additionalProperties.json --namespace=fixtures.additionalproperties
 call autorest %VANILLA_ARGUMENTS% --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/body-array.json --namespace=fixtures.bodyarray
@@ -43,6 +43,5 @@ call autorest %AZURE_ARGUMENTS% --input-file=https://raw.githubusercontent.com/A
 
 call autorest %ARM_ARGUMENTS% --input-file=https://raw.githubusercontent.com/Azure/autorest.testserver/master/swagger/lro.json --namespace=fixtures.lro
 del azure-tests\src\main\java\module-info.java
-del azure-tests\pom_arm.xml
 
 call autorest --use:. customization-tests/swagger
