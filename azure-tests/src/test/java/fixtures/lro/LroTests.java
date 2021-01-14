2d0cf0bb81df2c8194d376214ca87da325e6bdd6
Weidong Xu <weidxu@microsoft.com>
2021-01-14 17:14:50 +0800

    lro retry tests

--- azure-tests/src/test/java/fixtures/lro/LroTests.java
+++ azure-tests/src/test/java/fixtures/lro/LroTests.java
@@ -8,6 +8,9 @@ package fixtures.lro;
 import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.policy.CookiePolicy;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.http.policy.HttpLoggingPolicy;
 import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
 import com.azure.core.management.exception.ManagementException;
@@ -22,6 +25,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Locale;
 
 public class LroTests {
@@ -30,14 +34,18 @@ public class LroTests {
 
     @BeforeAll
     public static void setup() {
-        HttpPipeline pipeline = new HttpPipelineBuilder().policies(new UserAgentPolicy(), new RetryPolicy(), new CookiePolicy(), (context, next) -> {
-            String requestId = context.getHttpRequest().getHeaders().getValue("x-ms-client-request-id");
-            if (requestId == null) {
-                context.getHttpRequest().getHeaders().put("x-ms-client-request-id", "9C4D50EE-2D56-4CD3-8152-34347DC9F2B0");
-            }
-            return next.process();
-        }
-//        , new HttpLoggingPolicy(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+        HttpPipeline pipeline;
+        pipeline = new HttpPipelineBuilder().policies(new UserAgentPolicy(),
+                new RetryPolicy(),
+                new CookiePolicy(),
+                (context, next) -> {
+                    String requestId = context.getHttpRequest().getHeaders().getValue("x-ms-client-request-id");
+                    if (requestId == null) {
+                        context.getHttpRequest().getHeaders().put("x-ms-client-request-id", "9C4D50EE-2D56-4CD3-8152-34347DC9F2B0");
+                    }
+                    return next.process();
+                },
+                new HttpLoggingPolicy(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
         ).build();
 
         client = new AutoRestLongRunningOperationTestServiceBuilder()
@@ -358,15 +366,23 @@ public class LroTests {
     @Test
     public void putInlineComplete201() {
         ProductInner product = client.getLROs().put201Succeeded();
-        Assertions.assertNotNull(product.id());
+        Assertions.assertEquals("Succeeded", product.provisioningState());
     }
 
     @Test
     public void customHeaderPutAsyncSucceded() {
         // not work as testserver expected, Azure-AsyncOperation header is ignored.
+        /*
+        CustomHeaderPutAsyncSucceded
+        LROPutAsyncRetrySucceeded
+        LROPutAsyncNoRetrySucceeded
+        LROPutAsyncRetryFailed
+        LROPutAsyncNoRetryCanceled
+         */
+
 
         ProductInner product = client.getLrosCustomHeaders().putAsyncRetrySucceeded();
-        Assertions.assertNotNull(product.id());
+        Assertions.assertEquals("Succeeded", product.provisioningState());
     }
 
     @Test
@@ -377,11 +393,74 @@ public class LroTests {
     @Test
     public void customHeaderPutSucceeded() {
         ProductInner product = client.getLrosCustomHeaders().put201CreatingSucceeded200();
-        Assertions.assertNotNull(product.id());
+        Assertions.assertEquals("Succeeded", product.provisioningState());
     }
 
     @Test
     public void customHeaderPostSucceeded() {
         client.getLrosCustomHeaders().post202Retry200();
+    }
+
+    @Test
+    public void postDoubleHeadersFinalLocationGet() {
+        ProductInner product = client.getLROs().postDoubleHeadersFinalLocationGet();
+        Assertions.assertEquals("100", product.id());
+    }
+
+    @Test
+    @Disabled("final-state-via not supported")
+    public void postDoubleHeadersFinalAzureHeaderGet() {
+        ProductInner product = client.getLROs().postDoubleHeadersFinalAzureHeaderGet();
+        Assertions.assertEquals("100", product.id());
+    }
+
+    @Test
+    public void postDoubleHeadersFinalAzureHeaderGetDefault() {
+        ProductInner product = client.getLROs().postDoubleHeadersFinalAzureHeaderGetDefault();
+        Assertions.assertEquals("100", product.id());
+    }
+
+    @Test
+    public void post202List() {
+        List<ProductInner> products = client.getLROs().post202List();
+        Assertions.assertEquals("100", products.get(0).id());
+    }
+
+    @Test
+    public void retryPut201CreatingSucceeded200() {
+        ProductInner product = client.getLroRetrys().put201CreatingSucceeded200();
+        Assertions.assertEquals("100", product.id());
+    }
+
+    @Test
+    public void retryPutAsyncRelativeRetrySucceeded() {
+        ProductInner product = client.getLroRetrys().putAsyncRelativeRetrySucceeded();
+        Assertions.assertEquals("100", product.id());
+    }
+
+    @Test
+    public void retryDeleteProvisioning202Accepted200Succeeded() {
+        ProductInner product = client.getLroRetrys().deleteProvisioning202Accepted200Succeeded();
+        Assertions.assertEquals("100", product.id());
+    }
+
+    @Test
+    public void retryDelete202Retry200() {
+        client.getLroRetrys().delete202Retry200();
+    }
+
+    @Test
+    public void retryDeleteAsyncRelativeRetrySucceeded() {
+        client.getLroRetrys().deleteAsyncRelativeRetrySucceeded();
+    }
+
+    @Test
+    public void retryPost202Retry200() {
+        client.getLroRetrys().post202Retry200();
+    }
+
+    @Test
+    public void retryPostAsyncRelativeRetrySucceeded() {
+        client.getLroRetrys().postAsyncRelativeRetrySucceeded();
     }
 }
