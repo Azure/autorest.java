56a3b50e01e9f2ae30eef44f772cdc170948ac3b
Srikanta <srnagar@microsoft.com>
2021-01-19 10:16:49 -0800

    Revert xml annotation change

--- javagen/src/main/java/com/azure/autorest/mapper/ModelPropertyMapper.java
+++ javagen/src/main/java/com/azure/autorest/mapper/ModelPropertyMapper.java
@@ -86,7 +86,7 @@ public class ModelPropertyMapper implements IMapper<Property, ClientModelPropert
                 .xmlNamespace(xmlNamespace);
 
         List<String> annotationArgumentList = new ArrayList<String>() {{
-            add(String.format("value = \"%s\"", serializedName.toString()));
+            add(String.format("value = \"%s\"", xmlParamName));
         }};
         if (property.isRequired()) {
             annotationArgumentList.add("required = true");
