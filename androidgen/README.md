bb3aeebce0b7532ee43da4ec34dd7dbf29e46d28
Jianping Chen <jp.chen.jianping@gmail.com>
2021-03-09 13:33:33 -0800

    First cut setting up android plugin (#958)
    * First cut setting up android plugin
    
    * Update dependency and adapt to new interface
    
    * update dependency, version, and AndroidGen
    
    Co-authored-by: JP Chen <jiach@microsoft.com>

---
+++ androidgen/README.md
@@ -0,0 +1,19 @@
+#### Androidgen
+
+``` yaml
+use: $(this-folder)/../preprocessor
+pipeline:
+# --- extension remodeler ---
+  androidgen:
+    scope: android
+    input: preprocessor
+    output-artifact: java-files
+  
+  androidgen/emitter:
+    input: androidgen
+    scope: scope-androidgen/emitter
+scope-androidgen/emitter:
+    input-artifact: java-files
+    output-uri-expr: $key
+output-artifact: java-files
+```
---
+++ androidgen/src/main/java/com/azure/autorest/android/Main.java
@@ -0,0 +1,16 @@
+package com.azure.autorest.android;
+
+import com.azure.autorest.extension.base.jsonrpc.Connection;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connection connection = new Connection(System.out, System.in);
+        connection.dispatch("GetPluginNames", () -> Arrays.asList("androidgen"));
+        connection.dispatch("Process", (plugin, sessionId) -> new Androidgen(connection, plugin, sessionId).process(),
+                String.class, String.class);
+        connection.dispatchNotification("Shutdown", connection::stop);
+        connection.waitForAll();
+    }
+}
