fdc840c86c242840d7bfcddc800f0b78044dc418
JP Chen <jiach@microsoft.com>
2021-03-12 14:36:53 -0800

    Get impl function signature right

--- androidgen/src/main/java/com/azure/autorest/android/mapper/AndroidClientMethodMapper.java
+++ androidgen/src/main/java/com/azure/autorest/android/mapper/AndroidClientMethodMapper.java
@@ -1,14 +1,30 @@
 package com.azure.autorest.android.mapper;
 
 import com.azure.autorest.android.model.clientmodel.AndroidClientMethod;
+import com.azure.autorest.extension.base.model.codemodel.Operation;
 import com.azure.autorest.mapper.ClientMethodMapper;
 import com.azure.autorest.model.clientmodel.ClassType;
 import com.azure.autorest.model.clientmodel.ClientMethod;
+import com.azure.autorest.model.clientmodel.ClientMethodParameter;
 import com.azure.autorest.model.clientmodel.GenericType;
 import com.azure.autorest.model.clientmodel.IType;
+import com.azure.autorest.model.clientmodel.ProxyMethod;
+import com.azure.autorest.model.clientmodel.ReturnValue;
 
 public class AndroidClientMethodMapper extends ClientMethodMapper {
     private static ClientMethodMapper instance = new AndroidClientMethodMapper();
+
+    private static final ClientMethodParameter ANDROID_CONTEXT_PARAM = new ClientMethodParameter.Builder()
+            .description("The context to associate with this operation.")
+            .wireType(ClassType.AndroidContext)
+            .name("context")
+            .annotations(new java.util.ArrayList<>())
+            .isConstant(false)
+            .defaultValue(null)
+            .fromClient(false)
+            .isFinal(false)
+            .isRequired(false)
+            .build();
 
     protected AndroidClientMethodMapper() {
     }
@@ -23,21 +39,28 @@ public class AndroidClientMethodMapper extends ClientMethodMapper {
     }
 
     @Override
+    protected ClientMethodParameter getContextParameter() {
+        return ANDROID_CONTEXT_PARAM;
+    }
+
+    @Override
+    protected IType getContextType() {
+        return ClassType.AndroidContext;
+    }
+
+    @Override
     protected IType createPagedRestResponseReturnType(IType elementType) {
-        // REVISIT: temporary hack
-        return GenericType.AndroidPagedResponse(GenericType.AndroidPagedResponse(elementType));
+        return GenericType.AndroidCompletableFuture(GenericType.AndroidPagedResponse(elementType));
     }
 
     @Override
     protected IType createPagedAsyncReturnType(IType elementType) {
-        // REVISIT: temporary hack
-        return GenericType.AndroidCompletableFuture(GenericType.AndroidPagedResponse(elementType));
+        return GenericType.AndroidPagedResponse(elementType);
     }
 
     @Override
     protected IType createPagedSyncReturnType(IType elementType) {
-        // REVISIT: temporary hack
-        return GenericType.AndroidCompletableFuture(GenericType.AndroidPagedResponse(elementType));
+        return GenericType.AndroidPagedResponse(elementType);
     }
 
     @Override
@@ -56,7 +79,35 @@ public class AndroidClientMethodMapper extends ClientMethodMapper {
     }
 
     @Override
-    protected boolean shouldGeneratePagingMethods() {
-        return false;
+    protected IType createSyncReturnWithResponseType(IType syncReturnType) {
+        return (GenericType.AndroidResponse(syncReturnType));
     }
+
+    @Override
+    protected boolean shouldGeneratePagingMethods() {
+        return true;
+    }
+
+    @Override
+    protected ReturnValue createSimpleSyncRestResponseReturnValue(Operation operation, IType syncReturnWithResponse) {
+        return new ReturnValue(returnTypeDescription(operation, syncReturnWithResponse, syncReturnWithResponse), syncReturnWithResponse);
+    }
+
+    @Override
+    protected ReturnValue createSimpleAsyncRestResponseReturnValue(Operation operation, ProxyMethod proxyMethod, IType syncReturnType) {
+        IType asyncWithResponseType = GenericType.AndroidCompletableFuture(GenericType.AndroidResponse(syncReturnType));
+        return new ReturnValue(returnTypeDescription(operation, proxyMethod.getReturnType().getClientType(), syncReturnType),
+                asyncWithResponseType);
+    }
+
+    @Override
+    protected ReturnValue createSimpleSyncReturnValue(Operation operation, IType syncReturnType) {
+        return new ReturnValue(returnTypeDescription(operation, syncReturnType, syncReturnType), syncReturnType);
+    }
+
+    @Override
+    protected ReturnValue createSimpleAsyncReturnValue(Operation operation, IType asyncReturnType, IType syncReturnType) {
+        return new ReturnValue(returnTypeDescription(operation, asyncReturnType, syncReturnType), asyncReturnType);
+    }
+
 }
