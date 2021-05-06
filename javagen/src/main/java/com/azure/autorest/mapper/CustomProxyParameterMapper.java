167df4a1a8922bf0ce5938a102614ff9dda9bee1
Dragos Daian <v-ddaian@microsoft.com>
2021-05-06 16:02:09 +0200

    Update tab indentation.

--- javagen/src/main/java/com/azure/autorest/mapper/CustomProxyParameterMapper.java
+++ javagen/src/main/java/com/azure/autorest/mapper/CustomProxyParameterMapper.java
@@ -82,11 +82,11 @@ public class CustomProxyParameterMapper implements IMapper<Parameter, ProxyMetho
                 wireType = ClassType.String;
             }
         } else if (wireType instanceof ListType && parameter.getProtocol().getHttp().getIn() != RequestParameterLocation.Body /*&& parameter.getProtocol().getHttp().getIn() != RequestParameterLocation.FormData*/) {
-        	if (parameter.getProtocol().getHttp().getExplode()) {
+            if (parameter.getProtocol().getHttp().getExplode()) {
                 wireType = new ListType(ClassType.String);
-        	} else {
+            } else {
                 wireType = ClassType.String;
-        	}
+            }
         }
         if (parameter.getProtocol().getHttp().getExplode()) {
             builder.alreadyEncoded(true);
