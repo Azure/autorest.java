8b7fe2d40dfc3b6d7abf016dad6bad285b360990
Feiyue Yu <iamyfy@163.com>
2021-06-10 18:23:41 +0800

    PagingAsync

--- javagen/src/main/java/com/azure/autorest/template/protocol/ProtocolAsyncPagingMethodTemplate.java
+++ javagen/src/main/java/com/azure/autorest/template/protocol/ProtocolAsyncPagingMethodTemplate.java
@@ -1,7 +1,12 @@
 package com.azure.autorest.template.protocol;
 
-import com.azure.autorest.model.clientmodel.ClientMethod;
+import com.azure.autorest.extension.base.model.codemodel.RequestParameterLocation;
+import com.azure.autorest.extension.base.plugin.JavaSettings;
+import com.azure.autorest.model.clientmodel.*;
 import com.azure.autorest.model.javamodel.JavaClass;
+import com.azure.autorest.model.javamodel.JavaVisibility;
+
+import java.util.stream.Collectors;
 
 public class ProtocolAsyncPagingMethodTemplate extends ProtocolMethodBaseTemplate {
     private static ProtocolAsyncPagingMethodTemplate _instance = new ProtocolAsyncPagingMethodTemplate();
@@ -14,7 +19,88 @@ public class ProtocolAsyncPagingMethodTemplate extends ProtocolMethodBaseTemplat
     }
 
     @Override
-    public void write(ClientMethod model, JavaClass context) {
+    public void write(ClientMethod clientMethod, JavaClass typeBlock) {
+        // Java doc
+        generateJavadoc(clientMethod, typeBlock);
 
+        // Method name
+        String methodName;
+        if (clientMethod.getName().endsWith("Async")) {
+            methodName = clientMethod.getName().substring(0, clientMethod.getName().length() - "Async".length());
+        } else {
+            methodName = clientMethod.getName();
+        }
+
+        // Return type
+        String returnType = "PageFlux<BinaryData>";
+
+        // Annotation
+        typeBlock.annotation("ServiceMethod(returns = ReturnType.COLLECTION)");
+
+        // Arguments
+        StringBuilder methodArgsDeclare = new StringBuilder();
+        StringBuilder methodArgsInvoke = new StringBuilder();
+        for (ProxyMethodParameter p : clientMethod.getProxyMethod().getParameters()) {
+            if (p.getIsRequired() && !p.getFromClient() && !p.getIsConstant()) {
+                IType clientType = p.getClientType();
+                if (clientType instanceof ClassType || clientType instanceof EnumType) {
+                    clientType = ClassType.String;
+                }
+                if (p.getRequestParameterLocation() == RequestParameterLocation.Body) {
+                    clientType = ClassType.BinaryData;
+                }
+                methodArgsDeclare.append(clientType).append(" ").append(p.getName()).append(", ");
+                methodArgsInvoke.append(p.getName()).append(", ");
+            }
+        }
+        methodArgsDeclare.append("RequestOptions options");
+        methodArgsInvoke.append("options");
+
+        JavaVisibility visibility = JavaVisibility.Public;
+
+        // TODO: unknown function
+        if (JavaSettings.getInstance().isContextClientMethodParameter()) {
+            visibility = JavaVisibility.PackagePrivate;
+            methodArgsInvoke.append(", c");
+
+            // Actual method without context param
+            typeBlock.publicMethod(String.format("%s %s(%s)", returnType, methodName, methodArgsDeclare), methodBlock -> {
+                methodBlock.methodReturn(String.format("FluxUtil.withContext(c -> %s(%s))", methodName, methodArgsInvoke));
+            });
+            methodArgsDeclare.append(", Context context");
+        }
+
+        String methodSignature = String.format("%s %s(%s)", returnType, methodName, methodArgsDeclare);
+        typeBlock.method(visibility, null, methodSignature, methodBlock -> {
+            // Method body
+
+
+
+        });
+
+        if (clientMethod.getMethodPageDetails().nonNullNextLink()) {
+            writeMethod(typeBlock, clientMethod.getMethodVisibility(), clientMethod.getDeclaration(), function -> {
+                AddOptionalVariables(function, clientMethod, restAPIMethod.getParameters(), settings);
+                function.line("return new PagedFlux<>(");
+                function.indent(() -> {
+                    function.line("() -> %s(%s),",
+                            clientMethod.getProxyMethod().getPagingAsyncSinglePageMethodName(),
+                            clientMethod.getArgumentList());
+                    function.line("nextLink -> %s(%s));",
+                            clientMethod.getMethodPageDetails().getNextMethod().getProxyMethod().getPagingAsyncSinglePageMethodName(),
+                            clientMethod.getMethodPageDetails().getNextMethod().getArgumentList());
+                });
+            });
+        } else {
+            writeMethod(typeBlock, clientMethod.getMethodVisibility(), clientMethod.getDeclaration(), function -> {
+                AddOptionalVariables(function, clientMethod, restAPIMethod.getParameters(), settings);
+                function.line("return new PagedFlux<>(");
+                function.indent(() -> {
+                    function.line("() -> %s(%s));",
+                            clientMethod.getProxyMethod().getPagingAsyncSinglePageMethodName(),
+                            clientMethod.getArgumentList());
+                });
+            });
+        }
     }
 }
