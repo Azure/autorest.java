42e42abb0f63f2e77776a445b55bd8c1e845dd9c
Feiyue Yu <iamyfy@163.com>
2021-07-08 14:43:21 +0800

    Update pom, regenerate SDK (#1093)

--- protocol-tests/src/main/java/fixtures/bodycomplex/implementation/FlattencomplexesImpl.java
+++ protocol-tests/src/main/java/fixtures/bodycomplex/implementation/FlattencomplexesImpl.java
@@ -7,7 +7,7 @@ import com.azure.core.annotation.HostParam;
 import com.azure.core.annotation.ReturnType;
 import com.azure.core.annotation.ServiceInterface;
 import com.azure.core.annotation.ServiceMethod;
-import com.azure.core.http.RequestOptions;
+import com.azure.core.http.rest.RequestOptions;
 import com.azure.core.http.rest.Response;
 import com.azure.core.http.rest.RestProxy;
 import com.azure.core.util.BinaryData;
