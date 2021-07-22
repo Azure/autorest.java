1cbead8b8a54f499ac2ca5cb48c72d2a59e19921
Feiyue Yu <iamyfy@163.com>
2021-07-22 14:21:38 +0800

    Fix unchecked cast

--- protocol-tests/src/main/java/fixtures/paging/implementation/PagingsImpl.java
+++ protocol-tests/src/main/java/fixtures/paging/implementation/PagingsImpl.java
@@ -5045,10 +5045,9 @@ public final class PagingsImpl {
 
     private List<BinaryData> getValues(BinaryData binaryData, String path) {
         try {
-            Object obj = binaryData.toObject(Object.class);
-            Object values = ((Map) obj).get(path);
-            return (List<BinaryData>)
-                    (((List) values).stream().map(BinaryData::fromObject).collect(Collectors.toList()));
+            Map<?, ?> obj = binaryData.toObject(Map.class);
+            List<?> values = (List<?>) obj.get(path);
+            return values.stream().map(BinaryData::fromObject).collect(Collectors.toList());
         } catch (Exception e) {
             return null;
         }
@@ -5056,8 +5055,8 @@ public final class PagingsImpl {
 
     private String getNextLink(BinaryData binaryData, String path) {
         try {
-            Object obj = binaryData.toObject(Object.class);
-            return (String) ((Map) obj).get(path);
+            Map<?, ?> obj = binaryData.toObject(Map.class);
+            return (String) obj.get(path);
         } catch (Exception e) {
             return null;
         }
