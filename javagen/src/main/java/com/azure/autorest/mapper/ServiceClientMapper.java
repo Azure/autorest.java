abb76b55fe3f3ecd1bb75fa1ae7299ad481f2be1
Feiyue Yu <iamyfy@163.com>
2021-09-24 15:19:25 +0800

    Throw exception if no service version

--- javagen/src/main/java/com/azure/autorest/mapper/ServiceClientMapper.java
+++ javagen/src/main/java/com/azure/autorest/mapper/ServiceClientMapper.java
@@ -125,6 +125,9 @@ public class ServiceClientMapper implements IMapper<CodeModel, ServiceClient> {
             String serviceClientPropertyDefaultValueExpression = serviceClientPropertyClientType.defaultValueExpression(ClientModelUtil.getClientDefaultValueOrConstantValue(p));
 
             if (settings.isLowLevelClient() && serviceClientPropertyName.equals("apiVersion")) {
+                if (settings.getServiceVersions() == null || settings.getServiceVersions().isEmpty()) {
+                    throw new RuntimeException("Please set --service-versions parameter.");
+                }
                 String serviceName;
                 if (settings.getServiceName() == null) {
                     serviceName = serviceClientInterfaceName;
