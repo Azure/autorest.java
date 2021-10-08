6a780edbadc068d4e02d2f292d182cc6df74b23c
Weidong Xu <weidxu@microsoft.com>
2021-10-08 10:49:26 +0800

    set default model flatten configuration for fluent

--- fluent-tests/prepare-tests.bat
+++ fluent-tests/prepare-tests.bat
@@ -15,7 +15,7 @@ SET AUTOREST_CORE_VERSION=3.6.0
 SET MODELERFOUR_ARGUMENTS=--pipeline.modelerfour.additional-checks=false --pipeline.modelerfour.lenient-model-deduplication=true
 SET COMMON_ARGUMENTS=--java --use=../ --java.output-folder=./ %MODELERFOUR_ARGUMENTS% --azure-arm --java.license-header=MICROSOFT_MIT_SMALL
 SET FLUENT_ARGUMENTS=%COMMON_ARGUMENTS% --fluent --pipeline.modelerfour.flatten-payloads=true
-SET FLUENTLITE_ARGUMENTS=%COMMON_ARGUMENTS% --fluent=lite --payload-flattening-threshold=0 --pipeline.modelerfour.flatten-models=false --client-flattened-annotation-target=NONE --generate-samples
+SET FLUENTLITE_ARGUMENTS=%COMMON_ARGUMENTS% --fluent=lite --generate-samples
 
 REM fluent premium
 CALL autorest --version=%AUTOREST_CORE_VERSION% %FLUENT_ARGUMENTS% --input-file=https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/resources/resource-manager/Microsoft.Resources/stable/2019-08-01/resources.json --namespace=com.azure.mgmttest.resources
