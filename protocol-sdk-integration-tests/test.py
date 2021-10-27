867ecad2e657f1c98ed888d15f2c3679be60096b
Weidong Xu <weidxu@microsoft.com>
2021-10-27 16:31:16 +0800

    add simple verification of existance of expected files

--- protocol-sdk-integration-tests/test.py
+++ protocol-sdk-integration-tests/test.py
@@ -3,6 +3,7 @@
 import sys
 import platform
 import subprocess
+import shutil
 import logging
 from os import path
 
@@ -13,10 +14,18 @@ OS_WINDOWS = platform.system().lower() == 'windows'
 def main():
     script_path = path.abspath(path.dirname(sys.argv[0]))
 
+    purview_output_path = path.join(script_path, 'sdk/purview/azure-analytics-purview-account')
+
+    shutil.rmtree(purview_output_path, ignore_errors=True)
+
+    logging.info(f'delete {purview_output_path}')
+
     # generate code
-    cmd = f'autorest --input-file=https://github.com/Azure/azure-rest-api-specs/blob/main/specification/purview/data-plane/Azure.Analytics.Purview.Account/preview/2019-11-01-preview/account.json --use=../ --java --low-level-client --output-folder={script_path}/sdk/purview/azure-analytics-purview-account --namespace=com.azure.analytics.purview.account --credential-types=tokencredential --credential-scopes=https://purview.azure.net/.default --sdk-integration --generate-samples'.split(' ')
+    cmd = f'autorest --input-file=https://github.com/Azure/azure-rest-api-specs/blob/main/specification/purview/data-plane/Azure.Analytics.Purview.Account/preview/2019-11-01-preview/account.json --use=../ --java --low-level-client --output-folder={purview_output_path} --namespace=com.azure.analytics.purview.account --credential-types=tokencredential --credential-scopes=https://purview.azure.net/.default --sdk-integration --generate-samples'.split(' ')
     cmd[0] += ('.cmd' if OS_WINDOWS else '')
     subprocess.check_call(cmd, cwd=script_path)
+
+    logging.info('pass autorest')
 
     # build
     cmd = [
@@ -26,7 +35,24 @@ def main():
         '-Dmaven.javadoc.skip',
         '--no-transfer-progress'
     ]
-    subprocess.check_call(cmd, cwd=path.join(script_path, 'sdk/purview/azure-analytics-purview-account'))
+    subprocess.check_call(cmd, cwd=purview_output_path)
+
+    logging.info('pass maven package')
+
+    assert path.exists(path.join(purview_output_path, 'README.md'))
+    logging.info('pass README.md')
+
+    assert path.exists(path.join(purview_output_path, 'CHANGELOG.md'))
+    logging.info('pass CHANGELOG.md')
+
+    assert path.exists(path.join(purview_output_path, 'swagger/README_SPEC.md'))
+    logging.info('pass swagger/README_SPEC.md')
+
+    assert path.exists(path.join(purview_output_path, 'src/samples/java/com/azure/analytics/purview/account/generated'))
+    logging.info('pass samples')
+
+    assert path.exists(path.join(purview_output_path, 'src/test/java/com/azure/analytics/purview/account/ClientTests.java'))
+    logging.info('pass tests')
 
 
 if __name__ == "__main__":
