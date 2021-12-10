1637753b2bd55c695d97bf29f6e721450518f851
Weidong Xu <weidxu@microsoft.com>
2021-12-10 23:10:22 +0800

    mgmt, codegen report (#1288)

--- fluent-tests/gen_all.py
+++ fluent-tests/gen_all.py
@@ -116,7 +116,7 @@ def report_markdown(report_dir: str, results: List[CodegenResult]) -> None:
         logging.error(f'failure at build for PRs: {sdks_failure_build}')
 
     lines = []
-    lines.append('# Java Codegen Report')
+    lines.append('# Fluent Codegen Report')
     lines.append('Generated at ' + datetime.datetime.now().isoformat())
     lines.append('## Success')
     lines.append('<details>')
