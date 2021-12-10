9550e6bb484b75fb52d267b3ffe39c0be4adbcb6
XiaofeiCao <xiaofeicao@microsoft.com>
2021-12-10 09:23:11 +0800

    test code refactor

--- preprocessor/src/test/java/com/azure/autorest/MockPreprocessor.java
+++ preprocessor/src/test/java/com/azure/autorest/MockPreprocessor.java
@@ -9,8 +9,19 @@ import com.azure.autorest.extension.base.jsonrpc.Connection;
 import com.azure.autorest.extension.base.model.codemodel.CodeModel;
 import com.azure.autorest.extension.base.plugin.JavaSettings;
 import com.azure.autorest.preprocessor.Preprocessor;
+import com.azure.autorest.preprocessor.tranformer.Transformer;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.introspector.Property;
+import org.yaml.snakeyaml.nodes.NodeTuple;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Representer;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
@@ -35,6 +46,51 @@ public class MockPreprocessor extends Preprocessor {
         JavaSettingsAccessor.setHost(this);
     }
 
+    public CodeModel loadCodeModel(String fileName) {
+        String file = readFile(fileName);
+        CodeModel codeModel;
+        try {
+            if (!file.startsWith("{")) {
+                // YAML
+                codeModel = yamlMapper.loadAs(file, CodeModel.class);
+            } else {
+                codeModel = jsonMapper.readValue(file, CodeModel.class);
+            }
+        } catch (Exception e) {
+            System.err.println("Got an error " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        return codeModel;
+    }
+
+    public CodeModel transform(CodeModel codeModel) {
+        performPretransformUpdates(codeModel);
+        CodeModel result = new Transformer().transform(codeModel);
+        performPosttransformUpdates(codeModel);
+        return result;
+    }
+
+    public String dump(CodeModel codeModel) {
+        Representer representer = new Representer() {
+            @Override
+            protected NodeTuple representJavaBeanProperty(Object javaBean, Property property, Object propertyValue,
+                                                          Tag customTag) {
+                // if value of property is null, ignore it.
+                if (propertyValue == null) {
+                    return null;
+                }
+                else {
+                    return super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
+                }
+            }
+        };
+        LoaderOptions loaderOptions = new LoaderOptions();
+        loaderOptions.setMaxAliasesForCollections(Integer.MAX_VALUE);
+        Yaml newYaml = new Yaml(new Constructor(loaderOptions), representer, new DumperOptions(), loaderOptions);
+        return newYaml.dump(codeModel);
+    }
+
     /**
      * override default behavior from fetching settings through json-rpc to read locally
      * @param type
@@ -45,6 +101,23 @@ public class MockPreprocessor extends Preprocessor {
     @Override
     public <T> T getValue(Type type, String key) {
         return (T) SETTINGS_MAP.get(key);
+    }
+
+    @Override
+    public String readFile(String filename) {
+        try {
+            FileReader fileReader = new FileReader(filename);
+            BufferedReader reader = new BufferedReader(fileReader);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Yaml getYaml() {
--- preprocessor/src/test/java/com/azure/autorest/PreprocessorTest.java
+++ preprocessor/src/test/java/com/azure/autorest/PreprocessorTest.java
@@ -36,43 +36,13 @@ public class PreprocessorTest {
         MockPreprocessor preprocessor = new MockPreprocessor(new Connection(System.out, System.in), "dummy", "dummy");
         // This file can be obtained by calling generate commands from `generate` or `generate.bat`, it will appear under this project
         // It's a debug file from the output of modelerfour during the generate process
-        String file = readFile("code-model.yaml");
-        CodeModel codeModel = preprocessor.getYaml().loadAs(file, CodeModel.class);
+        String codeModelFileName = "code-model.yaml";
 
-        preprocessor.performPretransformUpdates(codeModel);
-        codeModel = new Transformer().transform(codeModel);
-        preprocessor.performPosttransformUpdates(codeModel);
+        CodeModel codeModel = preprocessor.loadCodeModel(codeModelFileName);
+        codeModel = preprocessor.transform(codeModel);
+        String output = preprocessor.dump(codeModel);
 
-        Representer representer = new Representer() {
-            @Override
-            protected NodeTuple representJavaBeanProperty(Object javaBean, Property property, Object propertyValue,
-                                                          Tag customTag) {
-                // if value of property is null, ignore it.
-                if (propertyValue == null) {
-                    return null;
-                }
-                else {
-                    return super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
-                }
-            }
-        };
-        LoaderOptions loaderOptions = new LoaderOptions();
-        loaderOptions.setMaxAliasesForCollections(Integer.MAX_VALUE);
-        Yaml newYaml = new Yaml(new Constructor(loaderOptions), representer, new DumperOptions(), loaderOptions);
-        String output = newYaml.dump(codeModel);
         System.out.println(output);
-    }
-
-    private String readFile(String filename) throws IOException {
-        FileReader fileReader = new FileReader(filename);
-        BufferedReader reader = new BufferedReader(fileReader);
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            sb.append(line);
-            sb.append("\n");
-        }
-        return sb.toString();
     }
 
 }
