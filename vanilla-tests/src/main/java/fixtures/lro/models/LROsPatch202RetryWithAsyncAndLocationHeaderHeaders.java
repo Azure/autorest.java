db9b22dba91e6a3c410b7611c212aae8aaf4e5e6
Weidong Xu <weidxu@microsoft.com>
2022-01-13 13:49:25 +0800

    regen lro test

---
+++ vanilla-tests/src/main/java/fixtures/lro/models/LROsPatch202RetryWithAsyncAndLocationHeaderHeaders.java
@@ -0,0 +1,67 @@
+package fixtures.lro.models;
+
+import com.azure.core.annotation.Fluent;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/** The LROsPatch202RetryWithAsyncAndLocationHeaderHeaders model. */
+@Fluent
+public final class LROsPatch202RetryWithAsyncAndLocationHeaderHeaders {
+    /*
+     * The Azure-AsyncOperation property.
+     */
+    @JsonProperty(value = "Azure-AsyncOperation")
+    private String azureAsyncOperation;
+
+    /*
+     * The Location property.
+     */
+    @JsonProperty(value = "Location")
+    private String location;
+
+    /**
+     * Get the azureAsyncOperation property: The Azure-AsyncOperation property.
+     *
+     * @return the azureAsyncOperation value.
+     */
+    public String getAzureAsyncOperation() {
+        return this.azureAsyncOperation;
+    }
+
+    /**
+     * Set the azureAsyncOperation property: The Azure-AsyncOperation property.
+     *
+     * @param azureAsyncOperation the azureAsyncOperation value to set.
+     * @return the LROsPatch202RetryWithAsyncAndLocationHeaderHeaders object itself.
+     */
+    public LROsPatch202RetryWithAsyncAndLocationHeaderHeaders setAzureAsyncOperation(String azureAsyncOperation) {
+        this.azureAsyncOperation = azureAsyncOperation;
+        return this;
+    }
+
+    /**
+     * Get the location property: The Location property.
+     *
+     * @return the location value.
+     */
+    public String getLocation() {
+        return this.location;
+    }
+
+    /**
+     * Set the location property: The Location property.
+     *
+     * @param location the location value to set.
+     * @return the LROsPatch202RetryWithAsyncAndLocationHeaderHeaders object itself.
+     */
+    public LROsPatch202RetryWithAsyncAndLocationHeaderHeaders setLocation(String location) {
+        this.location = location;
+        return this;
+    }
+
+    /**
+     * Validates the instance.
+     *
+     * @throws IllegalArgumentException thrown if the instance is not valid.
+     */
+    public void validate() {}
+}
