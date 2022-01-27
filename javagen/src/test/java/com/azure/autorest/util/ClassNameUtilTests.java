2b24788d3658d3d6efba86d80e4639e8381c7306
Weidong Xu <weidxu@microsoft.com>
2022-01-27 18:48:00 +0800

    more test

--- javagen/src/test/java/com/azure/autorest/util/ClassNameUtilTests.java
+++ javagen/src/test/java/com/azure/autorest/util/ClassNameUtilTests.java
@@ -10,6 +10,7 @@ public class ClassNameUtilTests {
 
     @Test
     public void testTruncateClassName() {
+        // limit class name
         String name = ClassNameUtil.truncateClassName(
                 "com.azure.resourcemanager.deviceprovisioningservices",
                 "src/samples/java",
@@ -17,6 +18,15 @@ public class ClassNameUtilTests {
                 "IotDpsResourceCheckProvisioningServiceNameAvailabilitySamples");
         Assert.assertEquals("IotDpsResourceCheckProvisioningServiceNameAvailabilit", name);
 
+        // do nothing as too little remaining length for class name
+        name = ClassNameUtil.truncateClassName(
+                "com.azure.resourcemanager.deviceprovisioningservicespadpadpadpadpadpad",
+                "src/samples/java",
+                "com.azure.resourcemanager.deviceprovisioningservicespadpadpadpadpadpad.generated",
+                "IotDpsResourceCheckProvisioningServiceNameAvailabilitySamples");
+        Assert.assertEquals("IotDpsResourceCheckProvisioningServiceNameAvailabilitySamples", name);
+
+        // no change
         name = ClassNameUtil.truncateClassName(
                 "com.azure.resourcemanager.datafactory",
                 "src/samples/java",
