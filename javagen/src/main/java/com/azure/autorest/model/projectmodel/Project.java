d7d1f6a692c98b158b137d76297efbd283f48414
Weidong Xu <weidxu@microsoft.com>
2022-02-08 11:29:41 +0800

    client, update core (#1345)

--- javagen/src/main/java/com/azure/autorest/model/projectmodel/Project.java
+++ javagen/src/main/java/com/azure/autorest/model/projectmodel/Project.java
@@ -50,11 +50,11 @@ public class Project {
 
     public static class PackageVersions {
         private String azureClientSdkParentVersion = "1.7.0";
-        private String azureCoreVersion = "1.24.1";
-        private String azureCoreManagementVersion = "1.5.1";
-        private String azureCoreHttpNettyVersion = "1.11.6";
-        private String azureCoreTestVersion = "1.7.7";
-        private String azureIdentityVersion = "1.4.3";
+        private String azureCoreVersion = "1.25.0";
+        private String azureCoreManagementVersion = "1.5.2";
+        private String azureCoreHttpNettyVersion = "1.11.7";
+        private String azureCoreTestVersion = "1.7.8";
+        private String azureIdentityVersion = "1.4.4";
         private String junitVersion = "5.8.2";
         private String revapiMavenPlugin = "0.14.6";
 
