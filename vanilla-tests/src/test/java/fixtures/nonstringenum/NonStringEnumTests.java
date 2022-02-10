8b9c706d7a0394bdfb16e7af29db93afd27f940f
Weidong Xu <weidxu@microsoft.com>
2022-02-10 12:51:47 +0800

    client, convert one test to disable-client-builder (#1347)

--- vanilla-tests/src/test/java/fixtures/nonstringenum/NonStringEnumTests.java
+++ vanilla-tests/src/test/java/fixtures/nonstringenum/NonStringEnumTests.java
@@ -1,5 +1,6 @@
 package fixtures.nonstringenum;
 
+import fixtures.nonstringenum.implementation.NonStringEnumsClientImpl;
 import fixtures.nonstringenum.models.FloatEnum;
 import fixtures.nonstringenum.models.IntEnum;
 import org.junit.BeforeClass;
@@ -8,32 +9,35 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class NonStringEnumTests {
-    private static NonStringEnumsClient client;
+    private static IntClient intClient;
+    private static FloatOperationClient floatClient;
 
     @BeforeClass
     public static void setup() {
-        client = new NonStringEnumsClientBuilder().buildClient();
+        NonStringEnumsClientImpl implClient = new NonStringEnumsClientImpl("http://localhost:3000");
+        intClient = new IntClient(implClient.getInts());
+        floatClient = new FloatOperationClient(implClient.getFloatOperations());
     }
 
     @Test
     public void getInt() {
-        IntEnum actual = client.getInts().get();
+        IntEnum actual = intClient.get();
         assertEquals(IntEnum.FOUR_HUNDRED_TWENTY_NINE, actual);
     }
 
     @Test
     public void putInt() {
-        client.getInts().put(IntEnum.TWO_HUNDRED);
+        intClient.put(IntEnum.TWO_HUNDRED);
     }
 
     @Test
     public void getFloat() {
-        FloatEnum actual = client.getFloatOperations().get();
+        FloatEnum actual = floatClient.get();
         assertEquals(FloatEnum.FOUR_HUNDRED_TWENTY_NINE1, actual);
     }
 
     @Test
     public void putFloat() {
-        client.getFloatOperations().put(FloatEnum.TWO_HUNDRED4);
+        floatClient.put(FloatEnum.TWO_HUNDRED4);
     }
 }
