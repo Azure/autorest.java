9c72ab0f22d91a5f720a212cff2578bc13448a20
Weidong Xu <weidxu@microsoft.com>
2022-03-03 19:14:11 +0800

    regen tests

--- protocol-tests/src/main/java/fixtures/header/AutoRestSwaggerBatHeaderServiceClientBuilder.java
+++ protocol-tests/src/main/java/fixtures/header/AutoRestSwaggerBatHeaderServiceClientBuilder.java
@@ -25,7 +25,6 @@ import com.azure.core.util.CoreUtils;
 import com.azure.core.util.serializer.JacksonAdapter;
 import fixtures.header.implementation.AutoRestSwaggerBatHeaderServiceClientImpl;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -41,7 +40,7 @@ public final class AutoRestSwaggerBatHeaderServiceClientBuilder {
 
     @Generated private static final String SDK_VERSION = "version";
 
-    @Generated private final Map<String, String> properties = new HashMap<>();
+    @Generated private final Map<String, String> properties = CoreUtils.getProperties("fixtures-header.properties");
 
     /** Create an instance of the AutoRestSwaggerBatHeaderServiceClientBuilder. */
     @Generated
