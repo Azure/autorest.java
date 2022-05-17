b1b75bb04a1f495be20b1af8eb0832d29a23bbbb
Weidong Xu <weidxu@microsoft.com>
2022-05-17 13:24:19 +0800

    add test

---
+++ protocol-tests/src/test/java/fixtures/constants/ConstantsTests.java
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package fixtures.constants;
+
+import com.azure.core.http.rest.RequestOptions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ConstantsTests {
+
+    private static AutoRestSwaggerConstantServiceClient client;
+
+    @BeforeAll
+    public static void setup() {
+        client = new AutoRestSwaggerConstantServiceClientBuilder().buildClient();
+    }
+
+    @Test
+    public void putClientConstantsTests() {
+        RequestOptions options = new RequestOptions()
+                // we should not set these parameters, as they are from client
+                .addQueryParam("query-constant", "100")
+                .addHeader("header-constant", "true");
+        Assertions.assertEquals(200, client.putClientConstantsWithResponse(options).getStatusCode());
+    }
+}
