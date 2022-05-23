ad5ce18a0f260c82e63e38bc0fab7e68f271b56d
Weidong Xu <weidxu@microsoft.com>
2022-05-23 13:32:41 +0800

    checkstyle

--- javagen/src/main/java/com/azure/autorest/model/clientmodel/ClientMethod.java
+++ javagen/src/main/java/com/azure/autorest/model/clientmodel/ClientMethod.java
@@ -20,13 +20,10 @@ import com.azure.core.http.rest.SimpleResponse;
 import com.azure.core.util.DateTimeRfc1123;
 
 import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
