ca3587baf5f3c2174847c62174ec3d4952559c65
Weidong Xu <weidxu@microsoft.com>
2022-07-04 15:18:43 +0800

    add confidentialledger

--- cadl-project/setup.sh
+++ cadl-project/setup.sh
@@ -1,6 +1,7 @@
 cd ../cadl-extension/
 npm run build
 cd ../cadl-project/
+mkdir node_modules/\@cadl-lang/java-client-emitter/
 tar -c --exclude node_modules -C ../cadl-extension/ . | tar -x -C node_modules/\@cadl-lang/java-client-emitter/
 
-cadl compile .
+cadl compile confidentialledger.cadl
