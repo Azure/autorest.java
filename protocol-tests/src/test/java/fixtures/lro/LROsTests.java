8416c13ac71aa050bfd2e7a7e2c1bf2c018c769b
Haoling Dong <haolingdong@microsoft.com>
2022-07-06 16:22:32 +0800

    Add test cases

--- protocol-tests/src/test/java/fixtures/lro/LROsTests.java
+++ protocol-tests/src/test/java/fixtures/lro/LROsTests.java
@@ -32,4 +32,94 @@ public class LROsTests {
         Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
         Assertions.assertTrue(poller.getFinalResult().toString().contains("\"provisioningState\": \"Succeeded\""));
     }
+
+    @Test
+    public void beginPatch200SucceededIgnoreHeaders() {
+        RequestOptions requestOptions = new RequestOptions()
+                .setBody(BinaryData.fromString("{\"location\": \"West US\"}"));
+        SyncPoller<BinaryData, BinaryData> poller =  client.beginPatch200SucceededIgnoreHeaders(requestOptions);
+        PollResponse<BinaryData> pollResponse = poller.waitForCompletion();
+        Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
+        Assertions.assertTrue(poller.getFinalResult().toString().contains("\"provisioningState\": \"Succeeded\""));
+    }
+
+    @Test
+    public void beginPatch201RetryOnlyAsyncHeader() {
+        RequestOptions requestOptions = new RequestOptions()
+                .setBody(BinaryData.fromString("{\"location\": \"West US\"}"));
+        SyncPoller<BinaryData, BinaryData> poller =  client.beginPatch201RetryWithAsyncHeader(requestOptions);
+        PollResponse<BinaryData> pollResponse = poller.waitForCompletion();
+        Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
+        Assertions.assertTrue(poller.getFinalResult().toString().contains("\"provisioningState\": \"Accepted\""));
+    }
+
+    @Test
+    public void beginPatch202RetryAsyncAndLocationHeader() {
+        RequestOptions requestOptions = new RequestOptions()
+                .setBody(BinaryData.fromString("{\"location\": \"West US\"}"));
+        SyncPoller<BinaryData, BinaryData> poller =  client.beginPatch202RetryWithAsyncAndLocationHeader(requestOptions);
+        PollResponse<BinaryData> pollResponse = poller.waitForCompletion();
+        Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
+        Assertions.assertTrue(poller.getFinalResult().toString().contains("\"provisioningState\": \"Succeeded\""));
+    }
+
+    @Test
+    public void beginPut201Succeeded() {
+        RequestOptions requestOptions = new RequestOptions()
+                .setBody(BinaryData.fromString("{\"location\": \"West US\"}"));
+        SyncPoller<BinaryData, BinaryData> poller =  client.beginPut201Succeeded(requestOptions);
+        PollResponse<BinaryData> pollResponse = poller.waitForCompletion();
+        Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
+        Assertions.assertTrue(poller.getFinalResult().toString().contains("\"provisioningState\": \"Succeeded\""));
+    }
+
+    @Test
+    public void beginPost202List() {
+        RequestOptions requestOptions = new RequestOptions()
+                .setBody(BinaryData.fromString("{\"location\": \"West US\"}"));
+        SyncPoller<BinaryData, BinaryData> poller =  client.beginPost202List(requestOptions);
+        PollResponse<BinaryData> pollResponse = poller.waitForCompletion();
+        Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
+        Assertions.assertTrue(poller.getFinalResult().toString().contains("\"name\": \"foo\""));
+    }
+
+    @Test
+    public void beginPut200SucceededNoState() {
+        RequestOptions requestOptions = new RequestOptions()
+                .setBody(BinaryData.fromString("{\"location\": \"West US\"}"));
+        SyncPoller<BinaryData, BinaryData> poller =  client.beginPut200SucceededNoState(requestOptions);
+        PollResponse<BinaryData> pollResponse = poller.waitForCompletion();
+        Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
+        Assertions.assertTrue(poller.getFinalResult().toString().contains("\"name\": \"foo\""));
+    }
+
+    @Test
+    public void beginPut202Retry200() {
+        RequestOptions requestOptions = new RequestOptions()
+                .setBody(BinaryData.fromString("{\"location\": \"West US\"}"));
+        SyncPoller<BinaryData, BinaryData> poller =  client.beginPut202Retry200(requestOptions);
+        PollResponse<BinaryData> pollResponse = poller.waitForCompletion();
+        Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
+        Assertions.assertTrue(pollResponse.getValue().toString().contains("\"name\": \"foo\""));
+    }
+
+    @Test
+    public void beginPutAsyncRetrySucceeded() {
+        RequestOptions requestOptions = new RequestOptions()
+                .setBody(BinaryData.fromString("{\"location\": \"West US\"}"));
+        SyncPoller<BinaryData, BinaryData> poller = client.beginPutAsyncRetrySucceeded(requestOptions);
+        PollResponse<BinaryData> pollResponse = poller.waitForCompletion();
+        Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
+        Assertions.assertTrue(poller.getFinalResult().toString().contains("\"provisioningState\": \"Succeeded\""));
+    }
+
+    @Test
+    public void beginNonRetryErrorPutAsyncRetry400() {
+        RequestOptions requestOptions = new RequestOptions()
+                .setBody(BinaryData.fromString("{\"location\": \"West US\"}"));
+        SyncPoller<BinaryData, BinaryData> poller =  client.beginPutAsyncRetryFailed(requestOptions);
+        PollResponse<BinaryData> pollResponse = poller.waitForCompletion();
+        Assertions.assertEquals(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, pollResponse.getStatus());
+        Assertions.assertTrue(poller.getFinalResult().toString().contains("\"provisioningState\": \"Failed\""));
+    }
 }
