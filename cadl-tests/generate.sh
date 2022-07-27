10845606a82c268009051dbef4349a01d5238fa6
Weidong Xu <weidxu@microsoft.com>
2022-07-27 14:36:31 +0800

    CI for cadl extension

--- cadl-tests/generate.sh
+++ cadl-tests/generate.sh
@@ -5,6 +5,9 @@ function cadl_compile {
 
     curl $1 --output tmp.cadl
     cadl compile tmp.cadl
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 
     cp -rf cadl-output/java/src/ .
     rm src/main/java/module-info.java
@@ -12,5 +15,7 @@ function cadl_compile {
     rm -rf cadl-output/java/src/
     rm tmp.cadl
 }
+
+rm -rf src/main/
 
 cadl_compile https://raw.githubusercontent.com/Azure/autorest.testserver/dpg_cadl/cadl/dpg-update1.cadl
