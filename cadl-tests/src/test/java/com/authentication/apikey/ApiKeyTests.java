c7a1a65cadce8d70710efaed051a639362f9f1de
Weidong Xu <weidxu@microsoft.com>
2022-09-13 13:15:25 +0800

    test for invalid

--- cadl-tests/src/test/java/com/authentication/apikey/ApiKeyTests.java
+++ cadl-tests/src/test/java/com/authentication/apikey/ApiKeyTests.java
@@ -4,6 +4,8 @@
 package com.authentication.apikey;
 
 import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.exception.HttpResponseException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ApiKeyTests {
@@ -16,5 +18,22 @@ public class ApiKeyTests {
                 .buildAsyncClient();
 
         client.valid().block();
+    }
+
+    @Test
+    public void testInvalid() {
+        AuthenticationApiKeyClient client = new AuthenticationApiKeyClientBuilder()
+                // AzureKeyCredentialPolicy from core requires HTTPS
+                .addPolicy(new AzureKeyCredentialPolicy("x-ms-api-key", new AzureKeyCredential("valid-key")))
+                .buildClient();
+
+        // assert HttpResponseException
+        Assertions.assertThrows(HttpResponseException.class, client::invalid);
+        // assert statusCode 403
+        try {
+            client.invalid();
+        } catch (HttpResponseException e) {
+            Assertions.assertEquals(403, e.getResponse().getStatusCode());
+        }
     }
 }
