4b8935c2586874e919af8814c659aa7f589f36de
XiaofeiCao <xiaofeicao@microsoft.com>
2022-09-27 11:28:57 +0800

    fix prepare-test.bat

--- fluent-tests/prepare-tests.bat
+++ fluent-tests/prepare-tests.bat
@@ -58,6 +58,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM swagger customed Resource and ProxyResource
 CALL autorest --version=%AUTOREST_CORE_VERSION% %FLUENT_ARGUMENTS% --input-file=https://github.com/Azure/azure-rest-api-specs/blob/main/specification/trafficmanager/resource-manager/Microsoft.Network/stable/2018-08-01/trafficmanager.json --namespace=com.azure.mgmttest.trafficmanager
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM fluent lite
 CALL autorest --version=%AUTOREST_CORE_VERSION% %FLUENTLITE_ARGUMENTS% --pom-file=pom_generated_resources.xml https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/resources/resource-manager/readme.md --tag=package-resources-2021-01 --java.namespace=com.azure.mgmtlitetest.resources
