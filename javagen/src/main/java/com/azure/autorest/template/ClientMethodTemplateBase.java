b6036bff9d3f8a71b62ee3c616d56da954a6e342
Haoling Dong <haolingdong@microsoft.com>
2022-10-11 11:26:01 +0800

    nit on exception description

--- javagen/src/main/java/com/azure/autorest/template/ClientMethodTemplateBase.java
+++ javagen/src/main/java/com/azure/autorest/template/ClientMethodTemplateBase.java
@@ -104,7 +104,7 @@ public abstract class ClientMethodTemplateBase implements IJavaTemplate<ClientMe
                             })
                             .findFirst().orElse(null);
                     if (responseBodyType == null) {
-                        throw new IllegalStateException(String.format("responseBodyType should not be null or void for paging method. rawResponseType = %s", rawResponseType.toString()));
+                        throw new IllegalStateException(String.format("responseBodyType should not be null for paging method. rawResponseType = %s", rawResponseType.toString()));
                     }
                 } else {
                     responseBodyType = clientMethod.getProxyMethod().getRawResponseBodyType();
