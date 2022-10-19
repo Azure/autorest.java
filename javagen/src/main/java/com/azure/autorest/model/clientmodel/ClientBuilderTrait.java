b1f3b6d92ce0623f27acf2c05af359ec896e873f
Haoling Dong <haolingdong@microsoft.com>
2022-10-19 18:35:14 +0800

    check customPolicy to be non null

--- javagen/src/main/java/com/azure/autorest/model/clientmodel/ClientBuilderTrait.java
+++ javagen/src/main/java/com/azure/autorest/model/clientmodel/ClientBuilderTrait.java
@@ -174,6 +174,7 @@ public class ClientBuilderTrait {
 
         // addPolicy
         Consumer<JavaBlock> addPolicyMethodImpl = function -> {
+            function.line("Objects.requireNonNull(customPolicy);");
             function.line("pipelinePolicies.add(customPolicy);");
             function.methodReturn("this");
         };
