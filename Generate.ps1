fae73f63718da5011eef253767429ccfba0b468d
Weidong Xu <weidxu@microsoft.com>
2022-10-19 08:01:06 +0800

    eng, try to see if "-UseNewRunspace" helps (#1779)

--- Generate.ps1
+++ Generate.ps1
@@ -70,7 +70,7 @@ if (Test-Path ./vanilla-tests/src/main) {
     "$VANILLA_ARGUMENTS --input-file=$SWAGGER_PATH/constants.json --namespace=fixtures.constants",
     "--version=$AUTOREST_CORE_VERSION --use=./ vanilla-tests/swagger/lro.md",
     "--version=$AUTOREST_CORE_VERSION --use=./ vanilla-tests/swagger/custom-http-exception-mapping.md"
-) | ForEach-Object -Parallel $generateScript
+) | ForEach-Object -Parallel $generateScript -UseNewRunspace
 
 if (Test-Path ./vanilla-tests/swagger/CoverageReporter.java) {
     Move-Item ./vanilla-tests/swagger/CoverageReporter.java ./vanilla-tests/src/main/java/fixtures/report/CoverageReporter.java -Force
@@ -91,7 +91,7 @@ if (Test-Path ./vanilla-tests/swagger/CoverageReporter.java) {
     "$VANILLA_ARGUMENTS --input-file=vanilla-tests/swagger/security-info.json --namespace=fixtures.securityinfo",
     "$VANILLA_ARGUMENTS --input-file=vanilla-tests/swagger/special-header.json --namespace=fixtures.specialheader",
     "$VANILLA_ARGUMENTS --input-file=vanilla-tests/swagger/required-fields-as-ctor-args-transformation.json --namespace=fixtures.requiredfieldsascotrargstransformation --required-fields-as-ctor-args=true --output-model-immutable"
-) | ForEach-Object -Parallel $generateScript
+) | ForEach-Object -Parallel $generateScript -UseNewRunspace
 
 # Azure
 @(
@@ -101,7 +101,7 @@ if (Test-Path ./vanilla-tests/swagger/CoverageReporter.java) {
     "$AZURE_ARGUMENTS --input-file=$SWAGGER_PATH/azure-parameter-grouping.json --namespace=fixtures.azureparametergrouping --payload-flattening-threshold=1",
     "$AZURE_ARGUMENTS --input-file=$SWAGGER_PATH/subscriptionId-apiVersion.json --namespace=fixtures.subscriptionidapiversion --payload-flattening-threshold=1",
     "$AZURE_ARGUMENTS --input-file=$SWAGGER_PATH/azure-report.json --namespace=fixtures.azurereport --payload-flattening-threshold=1"
-) | ForEach-Object -Parallel $generateScript
+) | ForEach-Object -Parallel $generateScript -UseNewRunspace
 
 #  "$ARM_ARGUMENTS --input-file=$SWAGGER_PATH/lro.json --namespace=fixtures.lro"
 #  "$ARM_ARGUMENTS --input-file=$SWAGGER_PATH/lro-parameterized-endpoints.json --namespace=fixtures.lroparameterizedendpoints"
@@ -135,7 +135,7 @@ Remove-Item ./protocol-tests/src/samples -Recurse -Force
     "$PROTOCOL_ARGUMENTS --input-file=protocol-tests/swagger/endpoint-lro.json --namespace=fixtures.endpointlro",
     "--version=$AUTOREST_CORE_VERSION --use=./ protocol-tests/swagger/dpg-customization.md",
     "--version=$AUTOREST_CORE_VERSION --use=./ protocol-tests/swagger/custom-http-exception-mapping.md"
-) | ForEach-Object -Parallel $generateScript
+) | ForEach-Object -Parallel $generateScript -UseNewRunspace
 
 New-Item ./protocol-tests/src/main/java/fixtures/headexceptions/models -ItemType Directory -Force
 Copy-Item -Path ./protocol-tests/swagger/CustomizedException.java -Destination ./protocol-tests/src/main/java/fixtures/headexceptions/models/CustomizedException.java -Force
@@ -148,7 +148,7 @@ Remove-Item ./protocol-resilience-test/llcupdate1/src/main -Recurse -Force
 @(
     "$PROTOCOL_RESILIENCE_ARGUMENTS --input-file=$SWAGGER_PATH/dpg-initial.json --namespace=fixtures.llcresi --output-folder=protocol-resilience-test/llcinitial",
     "$PROTOCOL_RESILIENCE_ARGUMENTS --input-file=$SWAGGER_PATH/dpg-update1.json --namespace=fixtures.llcresi --output-folder=protocol-resilience-test/llcupdate1"
-) | ForEach-Object -Parallel $generateScript
+) | ForEach-Object -Parallel $generateScript -UseNewRunspace
 
 Remove-Item ./protocol-resilience-test/llcinitial/src/main/java/module-info.java -Force
 Remove-Item ./protocol-resilience-test/llcupdate1/src/main/java/module-info.java -Force
@@ -165,6 +165,6 @@ Remove-Item ./partial-update-tests/generated/src/main/java/module-info.java -For
     "--use:. docs/samples/specification/azure_key_credential/readme.md",
     "--use:. docs/samples/specification/basic/readme.md",
     "--use:. docs/samples/specification/management/readme.md"
-) | ForEach-Object -Parallel $generateScript
+) | ForEach-Object -Parallel $generateScript -UseNewRunspace
 
 exit $ExitCode
