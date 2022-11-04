df643d4d6192d88f0cd25c3053a8d96e00328555
Weidong Xu <weidxu@microsoft.com>
2022-11-04 15:53:05 +0800

    client, update testserver (#1822)
    * update testserver
    
    * add BodyParam to reserved name

--- azure-tests/src/main/java/fixtures/paging/AutoRestPagingTestServiceAsyncClient.java
+++ azure-tests/src/main/java/fixtures/paging/AutoRestPagingTestServiceAsyncClient.java
@@ -73,6 +73,34 @@ public final class AutoRestPagingTestServiceAsyncClient {
     }
 
     /**
+     * A paging operation that finishes on the first call with body params without a nextlink.
+     *
+     * @param name The name parameter.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws HttpResponseException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return the paginated response with {@link PagedFlux}.
+     */
+    @Generated
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PagedFlux<Product> getSinglePagesWithBodyParams(String name) {
+        return this.serviceClient.getSinglePagesWithBodyParamsAsync(name);
+    }
+
+    /**
+     * A paging operation that finishes on the first call with body params without a nextlink.
+     *
+     * @throws HttpResponseException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return the paginated response with {@link PagedFlux}.
+     */
+    @Generated
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PagedFlux<Product> getSinglePagesWithBodyParams() {
+        return this.serviceClient.getSinglePagesWithBodyParamsAsync();
+    }
+
+    /**
      * A paging operation whose first response's items list is empty, but still returns a next link. Second (and final)
      * call, will give you an items list of 1.
      *
