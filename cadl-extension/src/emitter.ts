6ef07b49cb015f88e1a2285d256550322cad8c22
Weidong Xu <weidxu@microsoft.com>
2022-11-14 19:59:19 +0800

    cadl, report error if java not found on PATH (#1842)

--- cadl-extension/src/emitter.ts
+++ cadl-extension/src/emitter.ts
@@ -2,6 +2,7 @@ import {
   resolvePath,
   getNormalizedAbsolutePath,
   Program,
+  NoTarget,
   JSONSchemaType,
   createCadlLibrary,
 } from "@cadl-lang/compiler";
@@ -84,13 +85,28 @@ export async function $onEmit(program: Program, options: EmitterOptions) {
     const jarFileName = resolvePath(moduleRoot, "target", "azure-cadl-extension-jar-with-dependencies.jar");
     program.trace("cadl-java", `Exec JAR ${jarFileName}`);
 
-    const output = await promisify(execFile)("java", [
-      `-DemitterOptions=${emitterOptions}`,
-      "-jar",
-      jarFileName,
-      codeModelFileName,
-    ]);
-    program.trace("cadl-java", output.stdout ? output.stdout : output.stderr);
+    try {
+      const output = await promisify(execFile)("java", [
+        `-DemitterOptions=${emitterOptions}`,
+        "-jar",
+        jarFileName,
+        codeModelFileName,
+      ]);
+      program.trace("cadl-java", output.stdout ? output.stdout : output.stderr);
+    } catch (err: any) {
+      if ("code" in err && err.code === "ENOENT") {
+        const msg = "'java' is not on PATH. Please install JDK 11 or above.";
+        program.trace("cadl-java", msg);
+        program.reportDiagnostic({
+          code: "cadl-java",
+          severity: "error",
+          message: msg,
+          target: NoTarget,
+        });
+      } else {
+        throw err;
+      }
+    }
 
     if (!options["dev-options"]?.["generate-code-model"]) {
       await program.host.rm(codeModelFileName);
