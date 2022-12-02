57e90df852b708d50b5d6ae6ec4a604babb7f5d0
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2022-12-02 16:55:24 -0500

    Run Generate as a Job

--- Generate.ps1
+++ Generate.ps1
@@ -16,12 +16,22 @@ $generateScript = {
     $generateOutput = Invoke-Expression "autorest $_"
     $global:ExitCode = $global:ExitCode -bor $LASTEXITCODE
 
-    Write-Host "
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "
 ========================
 autorest $_
 ========================
 $([String]::Join("`n", $generateOutput))
-    "
+        "
+    } else {
+        Write-Host "
+========================
+autorest $_
+========================
+SUCCEEDED
+        "
+    }
+
     if ($global:ExitCode -ne 0) {
         exit $global:ExitCode
     }
@@ -30,14 +40,14 @@ $([String]::Join("`n", $generateOutput))
 java -version
 
 if (Test-Path ./vanilla-tests/src/main/java/fixtures/report/CoverageReporter.java) {
-    Move-Item -Path ./vanilla-tests/src/main/java/fixtures/report/CoverageReporter.java -Destination ./vanilla-tests/swagger/CoverageReporter.java -Force
+    Move-Item -Path ./vanilla-tests/src/main/java/fixtures/report/CoverageReporter.java -Destination ./vanilla-tests/swagger/CoverageReporter.java -Force | Out-Null
 }
 
 if (Test-Path ./vanilla-tests/src/main) {
-    Remove-Item ./vanilla-tests/src/main -Recurse -Force
+    Remove-Item ./vanilla-tests/src/main -Recurse -Force | Out-Null
 }
 
-@(
+$job = @(
     "$VANILLA_ARGUMENTS --input-file=$SWAGGER_PATH/additionalProperties.json --namespace=fixtures.additionalproperties",
     "$VANILLA_ARGUMENTS --input-file=$SWAGGER_PATH/body-array.json --namespace=fixtures.bodyarray",
     "$VANILLA_ARGUMENTS --input-file=$SWAGGER_PATH/body-boolean.json --namespace=fixtures.bodyboolean --client-logger",
@@ -78,14 +88,16 @@ if (Test-Path ./vanilla-tests/src/main) {
     "$VANILLA_ARGUMENTS --input-file=$SWAGGER_PATH/constants.json --namespace=fixtures.constants",
     "--version=$AUTOREST_CORE_VERSION --use=./ vanilla-tests/swagger/lro.md",
     "--version=$AUTOREST_CORE_VERSION --use=./ vanilla-tests/swagger/custom-http-exception-mapping.md"
-) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION
+) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION -AsJob
+$job | Wait-Job -Timeout 200 | Out-Null
+$job | Receive-Job
 
 if (Test-Path ./vanilla-tests/swagger/CoverageReporter.java) {
-    Move-Item ./vanilla-tests/swagger/CoverageReporter.java ./vanilla-tests/src/main/java/fixtures/report/CoverageReporter.java -Force
+    Move-Item ./vanilla-tests/swagger/CoverageReporter.java ./vanilla-tests/src/main/java/fixtures/report/CoverageReporter.java -Force | Out-Null
 }
 
 # local swagger
-@(
+$job = @(
     "$VANILLA_ARGUMENTS --input-file=vanilla-tests/swagger/discriminator-flattening.json --namespace=fixtures.discriminatorflattening --client-flattened-annotation-target=FIELD",
     "$VANILLA_ARGUMENTS --input-file=vanilla-tests/swagger/discriminator-flattening.json --namespace=fixtures.discriminatorflattening.requirexmsflattened --require-x-ms-flattened-to-flatten=true",
     "$VANILLA_ARGUMENTS --input-file=vanilla-tests/swagger/discriminator-flattening.json --namespace=fixtures.discriminatorflattening.noflatten --modelerfour.flatten-models=false",
@@ -99,27 +111,31 @@ if (Test-Path ./vanilla-tests/swagger/CoverageReporter.java) {
     "$VANILLA_ARGUMENTS --input-file=vanilla-tests/swagger/security-info.json --namespace=fixtures.securityinfo",
     "$VANILLA_ARGUMENTS --input-file=vanilla-tests/swagger/special-header.json --namespace=fixtures.specialheader",
     "$VANILLA_ARGUMENTS --input-file=vanilla-tests/swagger/required-fields-as-ctor-args-transformation.json --namespace=fixtures.requiredfieldsascotrargstransformation --required-fields-as-ctor-args=true --output-model-immutable"
-) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION
+) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION -AsJob
+$job | Wait-Job -Timeout 60 | Out-Null
+$job | Receive-Job
 
 # Azure
-@(
+$job = @(
     "$AZURE_ARGUMENTS --input-file=$SWAGGER_PATH/paging.json --namespace=fixtures.paging --payload-flattening-threshold=1 --generate-sync-async-clients",
     "$AZURE_ARGUMENTS --input-file=$SWAGGER_PATH/custom-baseUrl-paging.json --namespace=fixtures.custombaseuri.paging --payload-flattening-threshold=1",
     "$AZURE_ARGUMENTS --input-file=$SWAGGER_PATH/azure-special-properties.json --namespace=fixtures.azurespecials --payload-flattening-threshold=1",
     "$AZURE_ARGUMENTS --input-file=$SWAGGER_PATH/azure-parameter-grouping.json --namespace=fixtures.azureparametergrouping --payload-flattening-threshold=1",
     "$AZURE_ARGUMENTS --input-file=$SWAGGER_PATH/subscriptionId-apiVersion.json --namespace=fixtures.subscriptionidapiversion --payload-flattening-threshold=1",
     "$AZURE_ARGUMENTS --input-file=$SWAGGER_PATH/azure-report.json --namespace=fixtures.azurereport --payload-flattening-threshold=1"
-) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION
+) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION -AsJob
+$job | Wait-Job -Timeout 60 | Out-Null
+$job | Receive-Job
 
 #  "$ARM_ARGUMENTS --input-file=$SWAGGER_PATH/lro.json --namespace=fixtures.lro"
 #  "$ARM_ARGUMENTS --input-file=$SWAGGER_PATH/lro-parameterized-endpoints.json --namespace=fixtures.lroparameterizedendpoints"
 # Remove-Item ./azure-tests/src/main/java/module-info.java -Force
 
 # Protocol
-Remove-Item ./protocol-tests/src/main -Recurse -Force
-Remove-Item ./protocol-tests/src/samples -Recurse -Force
+Remove-Item ./protocol-tests/src/main -Recurse -Force | Out-Null
+Remove-Item ./protocol-tests/src/samples -Recurse -Force | Out-Null
 
-@(
+$job = @(
     "$PROTOCOL_ARGUMENTS --input-file=$SWAGGER_PATH/body-string.json --namespace=fixtures.bodystring",
     "$PROTOCOL_ARGUMENTS --input-file=$SWAGGER_PATH/paging.json --namespace=fixtures.paging",
     "$PROTOCOL_ARGUMENTS --input-file=$SWAGGER_PATH/body-complex.json --namespace=fixtures.bodycomplex",
@@ -143,37 +159,43 @@ Remove-Item ./protocol-tests/src/samples -Recurse -Force
     "$PROTOCOL_ARGUMENTS --input-file=protocol-tests/swagger/endpoint-lro.json --namespace=fixtures.endpointlro --service-name=LroEndpoint",
     "--version=$AUTOREST_CORE_VERSION --use=./ protocol-tests/swagger/dpg-customization.md",
     "--version=$AUTOREST_CORE_VERSION --use=./ protocol-tests/swagger/custom-http-exception-mapping.md"
-) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION
+) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION -AsJob
+$job | Wait-Job -Timeout 100 | Out-Null
+$job | Receive-Job
 
-New-Item ./protocol-tests/src/main/java/fixtures/headexceptions/models -ItemType Directory -Force
-Copy-Item -Path ./protocol-tests/swagger/CustomizedException.java -Destination ./protocol-tests/src/main/java/fixtures/headexceptions/models/CustomizedException.java -Force
-Remove-Item ./protocol-tests/src/main/java/module-info.java -Force
+New-Item ./protocol-tests/src/main/java/fixtures/headexceptions/models -ItemType Directory -Force | Out-Null
+Copy-Item -Path ./protocol-tests/swagger/CustomizedException.java -Destination ./protocol-tests/src/main/java/fixtures/headexceptions/models/CustomizedException.java -Force | Out-Null
+Remove-Item ./protocol-tests/src/main/java/module-info.java -Force | Out-Null
 
 # Protocol resilience
-Remove-Item ./protocol-resilience-test/llcinitial/src/main -Recurse -Force
-Remove-Item ./protocol-resilience-test/llcupdate1/src/main -Recurse -Force
+Remove-Item ./protocol-resilience-test/llcinitial/src/main -Recurse -Force | Out-Null
+Remove-Item ./protocol-resilience-test/llcupdate1/src/main -Recurse -Force | Out-Null
 
-@(
+$job = @(
     "$PROTOCOL_RESILIENCE_ARGUMENTS --input-file=$SWAGGER_PATH/dpg-initial.json --namespace=fixtures.llcresi --output-folder=protocol-resilience-test/llcinitial",
     "$PROTOCOL_RESILIENCE_ARGUMENTS --input-file=$SWAGGER_PATH/dpg-update1.json --namespace=fixtures.llcresi --output-folder=protocol-resilience-test/llcupdate1"
-) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION
+) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION -AsJob
+$job | Wait-Job -Timeout 30 | Out-Null
+$job | Receive-Job
 
-Remove-Item ./protocol-resilience-test/llcinitial/src/main/java/module-info.java -Force
-Remove-Item ./protocol-resilience-test/llcupdate1/src/main/java/module-info.java -Force
+Remove-Item ./protocol-resilience-test/llcinitial/src/main/java/module-info.java -Force | Out-Null
+Remove-Item ./protocol-resilience-test/llcupdate1/src/main/java/module-info.java -Force | Out-Null
 
 # customization
-Remove-Item ./customization-tests/src -Recurse -Force
+Remove-Item ./customization-tests/src -Recurse -Force | Out-Null
 "--version=$AUTOREST_CORE_VERSION --use:. customization-tests/swagger" | ForEach-Object $generateScript
 
 # partial update tests
 "--version=$AUTOREST_CORE_VERSION --use:. partial-update-tests/existing/swagger/README.md" | ForEach-Object $generateScript
-Remove-Item ./partial-update-tests/generated/src/main/java/module-info.java -Force
+Remove-Item ./partial-update-tests/generated/src/main/java/module-info.java -Force | Out-Null
 
 # docs
-@(
+$job = @(
     "--use:. docs/samples/specification/azure_key_credential/readme.md",
     "--use:. docs/samples/specification/basic/readme.md",
     "--use:. docs/samples/specification/management/readme.md"
-) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION
+) | ForEach-Object -Parallel $generateScript -ThrottleLimit $PARALLELIZATION -AsJob
+$job | Wait-Job -Timeout 30 | Out-Null
+$job | Receive-Job
 
 exit $ExitCode
