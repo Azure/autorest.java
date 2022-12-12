3c0e5a719113f97daf0acf8887ec789c3eb35bd5
Weidong Xu <weidxu@microsoft.com>
2022-12-12 14:41:32 +0800

    update script

--- cadl-tests/cadl_checkout.sh
+++ cadl-tests/cadl_checkout.sh
@@ -4,8 +4,8 @@
 export CADL_PATH=specification/cognitiveservices/OpenAI.Inference
 export CADL_BRANCH=feature/cognitiveservices/openai-cadl
 
-mkdir cadl_repo
-cd cadl_repo
+mkdir cadl-repo
+cd cadl-repo
 git init
 git config core.sparseCheckout true
 git remote add -f origin https://github.com/Azure/azure-rest-api-specs.git
@@ -13,5 +13,5 @@ echo $CADL_PATH > .git/info/sparse-checkout
 git checkout $CADL_BRANCH
 cd ..
 
-cp -rf cadl_repo/$CADL_PATH cadl_src
-rm -rf cadl_repo
+cp -rf cadl-repo/$CADL_PATH cadl-src
+rm -rf cadl-repo
