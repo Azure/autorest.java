139b3e3fc11a8cc55fe8b7e7d408aacedcd8c5ef
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-03-08 08:36:39 -0500

    Support Descriptions in Enum Values

--- extension-base/src/main/java/com/azure/autorest/extension/base/model/codemodel/SealedChoiceSchema.java
+++ extension-base/src/main/java/com/azure/autorest/extension/base/model/codemodel/SealedChoiceSchema.java
@@ -3,104 +3,19 @@
 
 package com.azure.autorest.extension.base.model.codemodel;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
 
 /**
  * a schema that represents a choice of several values (ie, an 'enum')
- * 
  */
-public class SealedChoiceSchema extends ValueSchema {
-
-    /**
-     * a Schema that represents a string value
-     * (Required)
-     * 
-     */
-    private Schema choiceType;
-    /**
-     * the possible choices for in the set
-     * (Required)
-     * 
-     */
-    private List<ChoiceValue> choices = new ArrayList<ChoiceValue>();
-    private String summary;
-
-    /**
-     * a Schema that represents a string value
-     * (Required)
-     * 
-     */
-    public Schema getChoiceType() {
-        return choiceType;
-    }
-
-    /**
-     * a Schema that represents a string value
-     * (Required)
-     * 
-     */
-    public void setChoiceType(Schema choiceType) {
-        this.choiceType = choiceType;
-    }
-
-    /**
-     * the possible choices for in the set
-     * (Required)
-     * 
-     */
-    public List<ChoiceValue> getChoices() {
-        return choices;
-    }
-
-    /**
-     * the possible choices for in the set
-     * (Required)
-     * 
-     */
-    public void setChoices(List<ChoiceValue> choices) {
-        this.choices = choices;
-    }
-
-    @Override
-    public String getSummary() {
-        return summary;
-    }
-
-    @Override
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
+public class SealedChoiceSchema extends ChoiceSchema {
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(SealedChoiceSchema.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
-        sb.append("choiceType");
-        sb.append('=');
-        sb.append(((this.choiceType == null)?"<null>":this.choiceType));
-        sb.append(',');
-        sb.append("choices");
-        sb.append('=');
-        sb.append(((this.choices == null)?"<null>":this.choices));
-        sb.append(',');
-        if (sb.charAt((sb.length()- 1)) == ',') {
-            sb.setCharAt((sb.length()- 1), ']');
-        } else {
-            sb.append(']');
-        }
-        return sb.toString();
+        return sharedToString(this, SealedChoiceSchema.class.getName());
     }
 
     @Override
     public int hashCode() {
-        int result = 1;
-        result = ((result* 31)+((this.choiceType == null)? 0 :this.choiceType.hashCode()));
-        result = ((result* 31)+((this.choices == null)? 0 :this.choices.hashCode()));
-        result = ((result* 31)+((this.getLanguage().getJava().getName() == null)? 0 :this.getLanguage().getJava().getName().hashCode()));
-        return result;
+        return sharedHashCode(this);
     }
 
     @Override
@@ -108,12 +23,11 @@ public class SealedChoiceSchema extends ValueSchema {
         if (other == this) {
             return true;
         }
-        if ((other instanceof SealedChoiceSchema) == false) {
+        if (!(other instanceof SealedChoiceSchema)) {
             return false;
         }
-        SealedChoiceSchema rhs = ((SealedChoiceSchema) other);
-        return Objects.equals(this.choiceType, rhs.choiceType) && Objects.equals(this.choices, rhs.choices)
-                && Objects.equals(this.getLanguage().getJava().getName(), rhs.getLanguage().getJava().getName());
+
+        return sharedEquals(this, (SealedChoiceSchema) other);
     }
 
 }
