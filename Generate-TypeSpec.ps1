2761dda9bb43f42d45f2e0340939d640a22f6ef5
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-03-09 10:45:28 -0500

    CADL -> TypeSpec

--- Generate-TypeSpec.ps1
+++ Generate-TypeSpec.ps1
@@ -1,6 +1,6 @@
 # Use case:
 #
-# The purpose of this script is to compact the steps required to regenerate CADL into a single script.
+# The purpose of this script is to compact the steps required to regenerate TypeSpec into a single script.
 #
 # Before running this script the 'cadl' profile must be built, 'mvn install -P local,cadl'.
 #
@@ -17,26 +17,26 @@ function invokeExpressionAndCaptureOutput([string]$expression) {
 Write-Host "Changing directory to './cadl-extension'"
 Set-Location ./cadl-extension
 
-Write-Host "Installing dependencies for CADL Java ('npm install')"
+Write-Host "Installing dependencies for TypeSpec Java ('npm install')"
 invokeExpressionAndCaptureOutput("npm install")
 
-Write-Host "Building CADL Java ('npm run build')"
+Write-Host "Building TypeSpec Java ('npm run build')"
 invokeExpressionAndCaptureOutput("npm run build")
 
-Write-Host "Linting CADL Java ('npm run lint')"
+Write-Host "Linting TypeSpec Java ('npm run lint')"
 invokeExpressionAndCaptureOutput("npm run lint")
 
-Write-Host "Checking CADL Java format ('npm run check-format')"
+Write-Host "Checking TypeSpec Java format ('npm run check-format')"
 invokeExpressionAndCaptureOutput("npm run check-format")
 
-Write-Host "Packing CADL Java ('npm pack')"
+Write-Host "Packing TypeSpec Java ('npm pack')"
 invokeExpressionAndCaptureOutput("npm pack")
 
 Write-Host "Returning to root directory ('..')"
 Set-Location ..
 
-Write-Host "Installing CADL ('npm install -g @cadl-lang/compiler')"
-invokeExpressionAndCaptureOutput("npm install -g @cadl-lang/compiler")
+Write-Host "Installing TypeSpec ('npm install -g @typespec/compiler')"
+invokeExpressionAndCaptureOutput("npm install -g @typespec/compiler")
 
 Write-Host "Changing directory to './cadl-tests'"
 Set-Location ./cadl-tests
