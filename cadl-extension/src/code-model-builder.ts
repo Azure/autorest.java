a48b6f4e338c4e708d776b941ff7e3c083cd3401
Weidong Xu <weidxu@microsoft.com>
2023-03-10 14:00:12 +0800

    Revert "check npm format report error"
    This reverts commit dd68d7384a267b390cd009b00f8f5ed4b4caa5f2.

--- cadl-extension/src/code-model-builder.ts
+++ cadl-extension/src/code-model-builder.ts
@@ -1000,7 +1000,9 @@ export class CodeModelBuilder {
 
   private getResponseDescription(resp: HttpOperationResponse): string {
     return (
-      resp.description || (resp.statusCode === "*" ? "An unexpected error response" : getStatusCodeDescription(resp.statusCode)) || ""
+      resp.description ||
+      (resp.statusCode === "*" ? "An unexpected error response" : getStatusCodeDescription(resp.statusCode)) ||
+      ""
     );
   }
 
