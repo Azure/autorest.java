7e7f357e87147c30ee5696969441f96c93279f4c
xiaofeicao <xiaofeicao@microsoft.com>
2023-04-07 16:06:31 +0800

    remove sync-stack.tsp

--- javagen/src/main/java/com/azure/autorest/util/ClientModelUtil.java
+++ javagen/src/main/java/com/azure/autorest/util/ClientModelUtil.java
@@ -31,7 +31,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
