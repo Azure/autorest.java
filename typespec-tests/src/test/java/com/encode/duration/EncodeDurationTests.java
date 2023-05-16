da49f44aa736d8f2aff897d81a985d5d740ece34
Weidong Xu <weidxu@microsoft.com>
2023-05-16 10:22:41 +0800

    test

---
+++ typespec-tests/src/test/java/com/encode/duration/EncodeDurationTests.java
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.encode.duration;
+
+import com.encode.duration.models.DefaultDurationProperty;
+import com.encode.duration.models.FloatSecondsDurationProperty;
+import com.encode.duration.models.Int32SecondsDurationProperty;
+import com.encode.duration.models.Iso8601DurationProperty;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+public class EncodeDurationTests {
+
+    private final QueryClient queryClient = new DurationClientBuilder().buildQueryClient();
+    private final PropertyClient propertyClient = new DurationClientBuilder().buildPropertyClient();
+    
+    private static final Duration DAY40 = Duration.ofDays(40);
+    private static final Duration SECOND35 = Duration.ofSeconds(35, 621_000_000);
+    private static final Duration SECOND36 = Duration.ofSeconds(36);
+
+    @Disabled
+    @Test
+    public void testQuery() {
+        queryClient.defaultMethod(DAY40);
+
+        queryClient.floatSeconds(SECOND35);
+
+        queryClient.int32Seconds(SECOND36);
+
+        queryClient.iso8601(DAY40);
+    }
+
+    @Test
+    public void testProperty() {
+        Assertions.assertEquals(DAY40,
+                propertyClient.defaultMethod(new DefaultDurationProperty(DAY40)).getValue());
+
+        Assertions.assertEquals(SECOND35,
+                propertyClient.floatSeconds(new FloatSecondsDurationProperty(SECOND35)).getValue());
+
+        Assertions.assertEquals(SECOND36,
+                propertyClient.int32Seconds(new Int32SecondsDurationProperty(SECOND36)).getValue());
+
+        propertyClient.iso8601(new Iso8601DurationProperty(DAY40));
+    }
+}
