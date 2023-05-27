a2176c0edbb1f0d2120652f75800e4d313b4b708
Weidong Xu <weidxu@microsoft.com>
2023-05-27 11:25:59 +0800

    regen

--- typespec-tests/src/main/java/com/cadl/versioning/VersioningClientBuilder.java
+++ typespec-tests/src/main/java/com/cadl/versioning/VersioningClientBuilder.java
@@ -30,6 +30,7 @@ import com.azure.core.util.ClientOptions;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.CoreUtils;
 import com.azure.core.util.builder.ClientBuilderUtil;
+import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.util.serializer.JacksonAdapter;
 import com.cadl.versioning.implementation.VersioningClientImpl;
 import java.util.ArrayList;
@@ -67,6 +68,9 @@ public final class VersioningClientBuilder
     @Generated
     @Override
     public VersioningClientBuilder pipeline(HttpPipeline pipeline) {
+        if (this.pipeline != null && pipeline == null) {
+            LOGGER.info("HttpPipeline is being set to 'null' when it was previously configured.");
+        }
         this.pipeline = pipeline;
         return this;
     }
@@ -266,4 +270,6 @@ public final class VersioningClientBuilder
     public VersioningClient buildClient() {
         return new VersioningClient(buildInnerClient());
     }
+
+    private static final ClientLogger LOGGER = new ClientLogger(VersioningClientBuilder.class);
 }
