b38bc074776f0d0c4973ed151902367836c7ce15
Srikanta Nagaraja <srnagar@microsoft.com>
2023-06-06 00:51:05 -0700

    change powershell to pwsh

--- typespec-tests/setup.py
+++ typespec-tests/setup.py
@@ -2,6 +2,6 @@ import subprocess
 import sys
 
 # A python wrapper to call the PowerShell Setup.ps1 script
-p = subprocess.Popen(["powershell.exe", ".\Setup.ps1"], stdout=sys.stdout)
+p = subprocess.Popen(["pwsh", ".\Setup.ps1"], stdout=sys.stdout)
 p.communicate()
 print("Setup complete")
