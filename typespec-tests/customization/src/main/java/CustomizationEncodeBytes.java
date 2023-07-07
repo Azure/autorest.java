82928cee4153ebea0207d0d20f4b3933ebe6de7f
Weidong Xu (from Dev Box) <weidxu@microsoft.com>
2023-07-07 14:47:53 +0800

    fix code

--- typespec-tests/customization/src/main/java/CustomizationEncodeBytes.java
+++ typespec-tests/customization/src/main/java/CustomizationEncodeBytes.java
@@ -12,11 +12,11 @@ import java.util.Arrays;
 /**
  * This class contains the customization code to customize the AutoRest generated code for App Configuration.
  */
-public class CustomizationTest extends Customization {
+public class CustomizationEncodeBytes extends Customization {
 
     @Override
     public void customize(LibraryCustomization customization, Logger logger) {
-        logger.info("Customizing the NamingClient javadoc");
+        logger.info("Customizing the Base64UrlArrayBytesProperty, add default private constructor");
         PackageCustomization packageCustomization = customization.getPackage("com.encode.bytes.models");
         ClassCustomization classCustomization = packageCustomization.getClass("Base64UrlArrayBytesProperty");
         classCustomization.addConstructor("@Generated private Base64UrlArrayBytesProperty() {}");
