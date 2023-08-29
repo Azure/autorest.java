29f1e11e0617dedd7b6a8f6afef0b2737ec41f61
Weidong Xu <weidxu@microsoft.com>
2023-08-29 15:10:47 +0800

    upgrade cadl-ranch

--- typespec-tests/src/main/java/com/type/union/UnionAsyncClient.java
+++ typespec-tests/src/main/java/com/type/union/UnionAsyncClient.java
@@ -136,4 +136,104 @@ public final class UnionAsyncClient {
         // Convenience API is not generated, as operation 'sendSecondNamedUnionValue' refers Union 'MyNamedUnion'
         return this.serviceClient.sendSecondNamedUnionValueWithResponseAsync(input, requestOptions);
     }
+
+    /**
+     * The receiveString operation.
+     *
+     * <p><strong>Response Body Schema</strong>
+     *
+     * <pre>{@code
+     * {
+     *     simpleUnion: Object (Required)
+     * }
+     * }</pre>
+     *
+     * @param requestOptions The options to configure the HTTP request before HTTP client sends it.
+     * @throws HttpResponseException thrown if the request is rejected by server.
+     * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
+     * @throws ResourceNotFoundException thrown if the request is rejected by server on status code 404.
+     * @throws ResourceModifiedException thrown if the request is rejected by server on status code 409.
+     * @return the response body along with {@link Response} on successful completion of {@link Mono}.
+     */
+    @Generated
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Response<BinaryData>> receiveStringWithResponse(RequestOptions requestOptions) {
+        // Convenience API is not generated, as operation 'receiveString' refers Union 'string | int32[]'
+        return this.serviceClient.receiveStringWithResponseAsync(requestOptions);
+    }
+
+    /**
+     * The receiveIntArray operation.
+     *
+     * <p><strong>Response Body Schema</strong>
+     *
+     * <pre>{@code
+     * {
+     *     simpleUnion: Object (Required)
+     * }
+     * }</pre>
+     *
+     * @param requestOptions The options to configure the HTTP request before HTTP client sends it.
+     * @throws HttpResponseException thrown if the request is rejected by server.
+     * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
+     * @throws ResourceNotFoundException thrown if the request is rejected by server on status code 404.
+     * @throws ResourceModifiedException thrown if the request is rejected by server on status code 409.
+     * @return the response body along with {@link Response} on successful completion of {@link Mono}.
+     */
+    @Generated
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Response<BinaryData>> receiveIntArrayWithResponse(RequestOptions requestOptions) {
+        // Convenience API is not generated, as operation 'receiveIntArray' refers Union 'string | int32[]'
+        return this.serviceClient.receiveIntArrayWithResponseAsync(requestOptions);
+    }
+
+    /**
+     * The receiveFirstNamedUnionValue operation.
+     *
+     * <p><strong>Response Body Schema</strong>
+     *
+     * <pre>{@code
+     * {
+     *     namedUnion: Object (Required)
+     * }
+     * }</pre>
+     *
+     * @param requestOptions The options to configure the HTTP request before HTTP client sends it.
+     * @throws HttpResponseException thrown if the request is rejected by server.
+     * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
+     * @throws ResourceNotFoundException thrown if the request is rejected by server on status code 404.
+     * @throws ResourceModifiedException thrown if the request is rejected by server on status code 409.
+     * @return the response body along with {@link Response} on successful completion of {@link Mono}.
+     */
+    @Generated
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Response<BinaryData>> receiveFirstNamedUnionValueWithResponse(RequestOptions requestOptions) {
+        // Convenience API is not generated, as operation 'receiveFirstNamedUnionValue' refers Union 'MyNamedUnion'
+        return this.serviceClient.receiveFirstNamedUnionValueWithResponseAsync(requestOptions);
+    }
+
+    /**
+     * The receiveSecondNamedUnionValue operation.
+     *
+     * <p><strong>Response Body Schema</strong>
+     *
+     * <pre>{@code
+     * {
+     *     namedUnion: Object (Required)
+     * }
+     * }</pre>
+     *
+     * @param requestOptions The options to configure the HTTP request before HTTP client sends it.
+     * @throws HttpResponseException thrown if the request is rejected by server.
+     * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
+     * @throws ResourceNotFoundException thrown if the request is rejected by server on status code 404.
+     * @throws ResourceModifiedException thrown if the request is rejected by server on status code 409.
+     * @return the response body along with {@link Response} on successful completion of {@link Mono}.
+     */
+    @Generated
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Response<BinaryData>> receiveSecondNamedUnionValueWithResponse(RequestOptions requestOptions) {
+        // Convenience API is not generated, as operation 'receiveSecondNamedUnionValue' refers Union 'MyNamedUnion'
+        return this.serviceClient.receiveSecondNamedUnionValueWithResponseAsync(requestOptions);
+    }
 }
