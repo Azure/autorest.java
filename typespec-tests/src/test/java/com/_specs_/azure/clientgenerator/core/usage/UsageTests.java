4bd2ba7e73c5d43d0e65e7ff587521f0c7308f23
Weidong Xu <weidxu@microsoft.com>
2023-09-06 14:46:17 +0800

    test

--- typespec-tests/src/test/java/com/_specs_/azure/clientgenerator/core/usage/UsageTests.java
+++ typespec-tests/src/test/java/com/_specs_/azure/clientgenerator/core/usage/UsageTests.java
@@ -4,16 +4,25 @@
 package com._specs_.azure.clientgenerator.core.usage;
 
 import com._specs_.azure.clientgenerator.core.usage.models.InputModel;
+import com._specs_.azure.clientgenerator.core.usage.models.OutputModel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 
 public class UsageTests {
 
     private final UsageClient client = new UsageClientBuilder().buildClient();
 
     @Test
-    public void test() {
+    public void test() throws NoSuchMethodException {
         client.inputToInputOutput(new InputModel("Madge"));
 
         client.outputToInputOutput();
+
+        // verify OutputModel has private constructor
+        Constructor<OutputModel> ctor = OutputModel.class.getDeclaredConstructor(String.class);
+        Assertions.assertEquals("private", Modifier.toString(ctor.getModifiers()));
     }
 }
