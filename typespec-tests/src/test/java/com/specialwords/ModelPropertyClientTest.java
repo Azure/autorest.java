5d55044263383016f0acfddc32404f0298c2f874
Weidong Xu <weidxu@microsoft.com>
2023-10-08 09:50:05 +0800

    disable

--- typespec-tests/src/test/java/com/specialwords/ModelPropertyClientTest.java
+++ typespec-tests/src/test/java/com/specialwords/ModelPropertyClientTest.java
@@ -4,12 +4,14 @@
 package com.specialwords;
 
 import com.specialwords.models.SameAsModel;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ModelPropertyClientTest {
 
     private final ModelPropertiesClient client = new SpecialWordsClientBuilder().buildModelPropertiesClient();
 
+    @Disabled
     @Test
     public void test() {
         client.sameAsModel(new SameAsModel("ok"));
