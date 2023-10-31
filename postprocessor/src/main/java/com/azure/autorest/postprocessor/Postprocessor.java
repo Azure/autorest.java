9a4bf461cf9fa0bd8175a1d41f6b0b42c662d042
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-10-31 13:32:48 -0400

    Fix linting

--- postprocessor/src/main/java/com/azure/autorest/postprocessor/Postprocessor.java
+++ postprocessor/src/main/java/com/azure/autorest/postprocessor/Postprocessor.java
@@ -29,7 +29,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
