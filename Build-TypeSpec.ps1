252a082e0c90e67d8729804fce8c5573f67e0d27
Weidong Xu <weidxu@microsoft.com>
2023-10-31 10:49:27 +0800

    update script

--- Build-TypeSpec.ps1
+++ Build-TypeSpec.ps1
@@ -1,7 +1,7 @@
 mvn package -P local,tsp -DskipTests
 
 Set-Location ./typespec-extension/
-npm install --force
+npm install
 npm run build
 npm run lint
 npm pack
--- openai-in-typespec/Build.ps1
+++ openai-in-typespec/Build.ps1
@@ -1,3 +1,15 @@
-npm install
+if (Test-Path node_modules) {
+    Remove-Item node_modules -Recurse -Force
+}
+
+if (Test-Path package-lock.json) {
+    Remove-Item package-lock.json
+}
+
+if (Test-Path openai-sdk) {
+    Remove-Item openai-sdk
+}
+
+npm install --force
 
 tsp compile main.tsp
