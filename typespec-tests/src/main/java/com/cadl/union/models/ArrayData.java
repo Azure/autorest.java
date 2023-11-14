12bbfc097deb81b723ac2d1964f28327a1f4a541
Weidong Xu <weidxu@microsoft.com>
2023-11-14 17:04:11 +0800

    tsp, fix NPE when using BinaryData (#2403)

--- typespec-tests/src/main/java/com/cadl/union/models/ArrayData.java
+++ typespec-tests/src/main/java/com/cadl/union/models/ArrayData.java
@@ -6,21 +6,24 @@ package com.cadl.union.models;
 
 import com.azure.core.annotation.Generated;
 import com.azure.core.annotation.Immutable;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * The ArrayData model.
  */
 @Immutable
-public final class ArrayData {
+public final class ArrayData implements JsonSerializable<ArrayData> {
     /*
      * The data property.
      */
     @Generated
-    @JsonProperty(value = "data")
-    private List<String> data;
+    private final List<String> data;
 
     /**
      * Creates an instance of ArrayData class.
@@ -28,8 +31,7 @@ public final class ArrayData {
      * @param data the data value to set.
      */
     @Generated
-    @JsonCreator
-    public ArrayData(@JsonProperty(value = "data") List<String> data) {
+    public ArrayData(List<String> data) {
         this.data = data;
     }
 
@@ -41,5 +43,51 @@ public final class ArrayData {
     @Generated
     public List<String> getData() {
         return this.data;
+    }
+
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeArrayField("data", this.data, (writer, element) -> writer.writeString(element));
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of ArrayData from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of ArrayData if the JsonReader was pointing to an instance of it, or null if it was pointing
+     * to JSON null.
+     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
+     * @throws IOException If an error occurs while reading the ArrayData.
+     */
+    public static ArrayData fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            boolean dataFound = false;
+            List<String> data = null;
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("data".equals(fieldName)) {
+                    data = reader.readArray(reader1 -> reader1.getString());
+                    dataFound = true;
+                } else {
+                    reader.skipChildren();
+                }
+            }
+            if (dataFound) {
+                ArrayData deserializedArrayData = new ArrayData(data);
+
+                return deserializedArrayData;
+            }
+            List<String> missingProperties = new ArrayList<>();
+            if (!dataFound) {
+                missingProperties.add("data");
+            }
+
+            throw new IllegalStateException(
+                "Missing required property/properties: " + String.join(", ", missingProperties));
+        });
     }
 }
--- typespec-tests/src/main/java/com/cadl/union/models/Result.java
+++ typespec-tests/src/main/java/com/cadl/union/models/Result.java
@@ -4,49 +4,50 @@
 
 package com.cadl.union.models;
 
+import com.azure.core.annotation.Fluent;
 import com.azure.core.annotation.Generated;
-import com.azure.core.annotation.Immutable;
 import com.azure.core.util.BinaryData;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The Result model.
  */
-@Immutable
-public final class Result {
+@Fluent
+public class Result implements JsonSerializable<Result> {
     /*
      * The name property.
      */
     @Generated
-    @JsonProperty(value = "name")
-    private String name;
+    private final String name;
 
     /*
      * The result property.
      */
     @Generated
-    @JsonProperty(value = "result")
     private Result result;
 
     /*
      * The data property.
      */
     @Generated
-    @JsonProperty(value = "data")
-    private BinaryData data;
+    private final BinaryData data;
 
     /**
      * Creates an instance of Result class.
      * 
      * @param name the name value to set.
-     * @param result the result value to set.
+     * @param data the data value to set.
      */
     @Generated
-    @JsonCreator
-    private Result(@JsonProperty(value = "name") String name, @JsonProperty(value = "result") Result result) {
+    public Result(String name, BinaryData data) {
         this.name = name;
-        this.result = result;
+        this.data = data;
     }
 
     /**
@@ -70,6 +71,18 @@ public final class Result {
     }
 
     /**
+     * Set the result property: The result property.
+     * 
+     * @param result the result value to set.
+     * @return the Result object itself.
+     */
+    @Generated
+    public Result setResult(Result result) {
+        this.result = result;
+        return this;
+    }
+
+    /**
      * Get the data property: The data property.
      * 
      * @return the data value.
@@ -77,5 +90,65 @@ public final class Result {
     @Generated
     public BinaryData getData() {
         return this.data;
+    }
+
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeStringField("name", this.name);
+        jsonWriter.writeUntypedField("data", this.data.toObject(Object.class));
+        jsonWriter.writeJsonField("result", this.result);
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of Result from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of Result if the JsonReader was pointing to an instance of it, or null if it was pointing to
+     * JSON null.
+     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
+     * @throws IOException If an error occurs while reading the Result.
+     */
+    public static Result fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            boolean nameFound = false;
+            String name = null;
+            boolean dataFound = false;
+            BinaryData data = null;
+            Result result = null;
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("name".equals(fieldName)) {
+                    name = reader.getString();
+                    nameFound = true;
+                } else if ("data".equals(fieldName)) {
+                    data = BinaryData.fromObject(reader.readUntyped());
+                    dataFound = true;
+                } else if ("result".equals(fieldName)) {
+                    result = Result.fromJson(reader);
+                } else {
+                    reader.skipChildren();
+                }
+            }
+            if (nameFound && dataFound) {
+                Result deserializedResult = new Result(name, data);
+                deserializedResult.result = result;
+
+                return deserializedResult;
+            }
+            List<String> missingProperties = new ArrayList<>();
+            if (!nameFound) {
+                missingProperties.add("name");
+            }
+            if (!dataFound) {
+                missingProperties.add("data");
+            }
+
+            throw new IllegalStateException(
+                "Missing required property/properties: " + String.join(", ", missingProperties));
+        });
     }
 }
