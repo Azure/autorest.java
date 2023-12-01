5166e20eaecbc9dcffb4b40b02a59383bed8d6ac
hongli750210 <v-hongli1@microsoft.com>
2023-12-01 17:11:13 +0800

    Add test cases for Projection_ProjectedName_Model_client and Projection_ProjectedName_Model_language

--- typespec-tests/src/test/java/com/projection/projectedname/ProjectedNameClientTest.java
+++ typespec-tests/src/test/java/com/projection/projectedname/ProjectedNameClientTest.java
@@ -3,16 +3,19 @@
 
 package com.projection.projectedname;
 
+import com.projection.projectedname.models.ClientModel;
 import com.projection.projectedname.models.ClientProjectedNameModel;
 import com.projection.projectedname.models.JsonAndClientProjectedNameModel;
 import com.projection.projectedname.models.JsonProjectedNameModel;
 import com.projection.projectedname.models.LanguageProjectedNameModel;
+import com.projection.projectedname.models.JavaModel;
 import org.junit.jupiter.api.Test;
 
 class ProjectedNameClientTest {
 
     private final PropertyClient propertyClient = new ProjectedNameClientBuilder().buildPropertyClient();
     private final ProjectedNameClient client = new ProjectedNameClientBuilder().buildClient();
+    private final ModelClient modelClient = new ProjectedNameClientBuilder().buildModelClient();
 
     @Test
     void jsonProjection() {
@@ -36,6 +39,18 @@ class ProjectedNameClientTest {
     void languageProjection() {
         LanguageProjectedNameModel project = new LanguageProjectedNameModel(true);
         propertyClient.language(project);
+    }
+
+    @Test
+    void clientModel() {
+        ClientModel clientModel = new ClientModel(true);
+        modelClient.client(clientModel);
+    }
+
+    @Test
+    void languageModel() {
+        JavaModel javaModel = new JavaModel(true);
+        modelClient.language(javaModel);
     }
 
     @Test
