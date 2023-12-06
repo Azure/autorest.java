1eb236935c3bbf69a54d0e38df76537421a4b36f
Weidong Xu <weidxu@microsoft.com>
2023-12-06 17:20:55 +0800

    tsp, add missing decimal test for cadl-ranch (#2449)
    * tsp, add missing decimal test for cadl-ranch
    
    * update DecimalTests to reuse generated DecimalProperty

--- typespec-tests/src/test/java/com/type/scalar/DecimalTests.java
+++ typespec-tests/src/test/java/com/type/scalar/DecimalTests.java
@@ -3,98 +3,69 @@
 
 package com.type.scalar;
 
+import com.azure.core.annotation.Immutable;
 import com.azure.core.util.BinaryData;
-import com.azure.json.JsonReader;
-import com.azure.json.JsonSerializable;
-import com.azure.json.JsonToken;
-import com.azure.json.JsonWriter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.type.property.valuetypes.models.DecimalProperty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 
 public class DecimalTests {
 
-    public interface Getter {
-        BigDecimal getProperty();
-    }
-
-    public static class DecimalStream implements JsonSerializable<DecimalStream>, Getter {
-        private BigDecimal property;
-
-        public BigDecimal getProperty() {
-            return property;
-        }
-
-        public DecimalStream setProperty(BigDecimal property) {
-            this.property = property;
-            return this;
-        }
-
-        @Override
-        public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
-            jsonWriter.writeStartObject();
-            jsonWriter.writeNumberField("property", this.property == null ? null : this.property);
-            return jsonWriter.writeEndObject();
-        }
-
-        public static DecimalStream fromJson(JsonReader jsonReader) throws IOException {
-            return jsonReader.readObject(reader -> {
-                DecimalStream decimalProperty = new DecimalStream();
-                while (reader.nextToken() != JsonToken.END_OBJECT) {
-                    String fieldName = reader.getFieldName();
-                    reader.nextToken();
-
-                    if ("property".equals(fieldName)) {
-                        BigDecimal property = reader.getNullable(nonNullReader -> new BigDecimal(nonNullReader.getString()));
-                        decimalProperty.setProperty(property);
-                    } else {
-                        reader.skipChildren();
-                    }
-                }
-                return decimalProperty;
-            });
-        }
-    }
-
-    public static class DecimalJackson implements Getter {
+    @Immutable
+    public static class DecimalJackson {
         @JsonProperty("property")
         private BigDecimal property;
 
-        @Override
-        public BigDecimal getProperty() {
-            return property;
+        public DecimalJackson(@JsonProperty("property") BigDecimal property) {
+            this.property = property;
         }
 
-        public void setProperty(BigDecimal property) {
-            this.property = property;
+        public BigDecimal getProperty() {
+            return property;
         }
     }
 
     @ParameterizedTest
-    @ValueSource(classes = {DecimalStream.class, DecimalJackson.class})
-    public <T extends Getter> void testBigDecimal(Class<T> clazz) {
+    @ValueSource(classes = {DecimalProperty.class, DecimalJackson.class})
+    public <T> void testBigDecimal(Class<T> clazz) {
         // precision larger than double
         BigDecimal value = new BigDecimal("12345678901234567890.1234567890");
-        String json = BinaryData.fromObject(new DecimalStream().setProperty(value)).toString();
+        String json = BinaryData.fromObject(newInstance(clazz, value)).toString();
         var test = BinaryData.fromString(json).toObject(clazz);
-        Assertions.assertEquals(value, test.getProperty());
+        Assertions.assertEquals(value, getProperty(clazz, test));
 
         // make sure precision difference would cause NotEquals
         Assertions.assertNotEquals(value, new BigDecimal("12345678901234567890.123456789"));
 
         // scientific
         value = new BigDecimal("1.2345678901234567890E23");
-        json = BinaryData.fromObject(new DecimalStream().setProperty(value)).toString();
+        json = BinaryData.fromObject(newInstance(clazz, value)).toString();
         test = BinaryData.fromString(json).toObject(clazz);
-        Assertions.assertEquals(value, test.getProperty());
+        Assertions.assertEquals(value, getProperty(clazz, test));
 
         value = new BigDecimal("-1.2345678901234567890e-105");
-        json = BinaryData.fromObject(new DecimalStream().setProperty(value)).toString();
+        json = BinaryData.fromObject(newInstance(clazz, value)).toString();
         test = BinaryData.fromString(json).toObject(clazz);
-        Assertions.assertEquals(value, test.getProperty());
+        Assertions.assertEquals(value, getProperty(clazz, test));
+    }
+
+    private static <T> T newInstance(Class<T> clazz, BigDecimal value) {
+        try {
+            return clazz.getDeclaredConstructor(BigDecimal.class).newInstance(value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static <T> BigDecimal getProperty(Class<T> clazz, T obj) {
+        try {
+            return (BigDecimal) clazz.getDeclaredMethod("getProperty").invoke(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
