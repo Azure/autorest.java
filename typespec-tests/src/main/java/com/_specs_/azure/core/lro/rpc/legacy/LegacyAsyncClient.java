7086829636c6822350eabab616ebe3c0e3fda01a
Weidong Xu <weidxu@microsoft.com>
2023-12-11 12:46:41 +0800

    regen

--- typespec-tests/src/main/java/com/_specs_/azure/core/lro/rpc/legacy/LegacyAsyncClient.java
+++ typespec-tests/src/main/java/com/_specs_/azure/core/lro/rpc/legacy/LegacyAsyncClient.java
@@ -16,8 +16,11 @@ import com.azure.core.exception.HttpResponseException;
 import com.azure.core.exception.ResourceModifiedException;
 import com.azure.core.exception.ResourceNotFoundException;
 import com.azure.core.http.rest.RequestOptions;
+import com.azure.core.http.rest.Response;
 import com.azure.core.util.BinaryData;
+import com.azure.core.util.FluxUtil;
 import com.azure.core.util.polling.PollerFlux;
+import reactor.core.publisher.Mono;
 
 /**
  * Initializes a new instance of the asynchronous LegacyClient type.
@@ -35,6 +38,52 @@ public final class LegacyAsyncClient {
     @Generated
     LegacyAsyncClient(CreateResourcePollViaOperationLocationsImpl serviceClient) {
         this.serviceClient = serviceClient;
+    }
+
+    /**
+     * Poll a Job.
+     * <p>
+     * <strong>Response Body Schema</strong>
+     * </p>
+     * <pre>{@code
+     * {
+     *     jobId: String (Required)
+     *     comment: String (Required)
+     *     status: String(notStarted/running/succeeded/failed/canceled/partiallyCompleted) (Required)
+     *     errors (Optional): [
+     *          (Optional){
+     *             error (Required): {
+     *                 code: String (Required)
+     *                 message: String (Required)
+     *                 target: String (Optional)
+     *                 details (Optional): [
+     *                     (recursive schema, see above)
+     *                 ]
+     *                 innererror (Optional): {
+     *                     code: String (Optional)
+     *                     innererror (Optional): (recursive schema, see innererror above)
+     *                 }
+     *             }
+     *         }
+     *     ]
+     *     results (Optional): [
+     *         String (Optional)
+     *     ]
+     * }
+     * }</pre>
+     * 
+     * @param jobId A processing job identifier.
+     * @param requestOptions The options to configure the HTTP request before HTTP client sends it.
+     * @throws HttpResponseException thrown if the request is rejected by server.
+     * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
+     * @throws ResourceNotFoundException thrown if the request is rejected by server on status code 404.
+     * @throws ResourceModifiedException thrown if the request is rejected by server on status code 409.
+     * @return result of the job along with {@link Response} on successful completion of {@link Mono}.
+     */
+    @Generated
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Response<BinaryData>> getJobWithResponse(String jobId, RequestOptions requestOptions) {
+        return this.serviceClient.getJobWithResponseAsync(jobId, requestOptions);
     }
 
     /**
@@ -89,6 +138,27 @@ public final class LegacyAsyncClient {
     @ServiceMethod(returns = ReturnType.LONG_RUNNING_OPERATION)
     public PollerFlux<BinaryData, BinaryData> beginCreateJob(BinaryData jobData, RequestOptions requestOptions) {
         return this.serviceClient.beginCreateJobAsync(jobData, requestOptions);
+    }
+
+    /**
+     * Poll a Job.
+     * 
+     * @param jobId A processing job identifier.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws HttpResponseException thrown if the request is rejected by server.
+     * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
+     * @throws ResourceNotFoundException thrown if the request is rejected by server on status code 404.
+     * @throws ResourceModifiedException thrown if the request is rejected by server on status code 409.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     * @return result of the job on successful completion of {@link Mono}.
+     */
+    @Generated
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<JobResult> getJob(String jobId) {
+        // Generated convenience method for getJobWithResponse
+        RequestOptions requestOptions = new RequestOptions();
+        return getJobWithResponse(jobId, requestOptions).flatMap(FluxUtil::toMono)
+            .map(protocolMethodData -> protocolMethodData.toObject(JobResult.class));
     }
 
     /**
