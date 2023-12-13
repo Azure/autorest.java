8c72ed64834e22cfebcf49fab81b29d90bf9d5ee
Weidong Xu <weidxu@microsoft.com>
2023-12-13 11:20:56 +0800

    tsp, adapt finalResult from LroMetadata (#2458)
    * use LroMetadata.finalResult
    
    * keep consistency with DPG 1.0; finalResult be Void only if httpMethod is DELETE

--- javagen/src/main/java/com/azure/autorest/mapper/ClientMethodMapper.java
+++ javagen/src/main/java/com/azure/autorest/mapper/ClientMethodMapper.java
@@ -437,14 +437,19 @@ public class ClientMethodMapper implements IMapper<Operation, List<ClientMethod>
                         // a new method to be added as implementation only (not exposed to client) for developer
                         dpgMethodPollingDetailsWithModel = methodPollingDetails;
 
+                        // keep consistency with DPG from Swagger, see getPollingFinalType
+                        IType resultType = ClassType.BINARY_DATA;
+                        // DELETE would not have final response as resource is deleted
+                        if (MethodUtil.getHttpMethod(operation) == HttpMethod.DELETE) {
+                            resultType = PrimitiveType.VOID;
+                        }
+
                         // DPG keep the method with BinaryData
                         methodPollingDetails = new MethodPollingDetails(
                             dpgMethodPollingDetailsWithModel.getPollingStrategy(),
                             dpgMethodPollingDetailsWithModel.getSyncPollingStrategy(),
                             ClassType.BINARY_DATA,
-                            // if model says final type is Void, then it is Void
-                            (dpgMethodPollingDetailsWithModel.getFinalType().asNullable() == ClassType.VOID) ? PrimitiveType.VOID
-                                : ClassType.BINARY_DATA,
+                            resultType,
                             dpgMethodPollingDetailsWithModel.getPollIntervalInSeconds());
                     }
 
