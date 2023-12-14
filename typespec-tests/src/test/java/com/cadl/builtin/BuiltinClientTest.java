d1aa3205d98d1aa860164d3e1764e7e2b14c3cfa
Haoling Dong <haolingdong@microsoft.com>
2023-12-14 11:02:34 +0800

    add case on serialization for null model map property

--- typespec-tests/src/test/java/com/cadl/builtin/BuiltinClientTest.java
+++ typespec-tests/src/test/java/com/cadl/builtin/BuiltinClientTest.java
@@ -5,6 +5,7 @@ package com.cadl.builtin;
 
 import com.azure.core.util.BinaryData;
 import com.cadl.builtin.models.Builtin;
+import com.type.dictionary.models.InnerModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,13 +14,22 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.HashMap;
+import java.util.Map;
 
 class BuiltinClientTest {
     @Test
-    public void testSerializationForMapProperty() {
+    public void testSerializationForFloatMapProperty() {
         Builtin model = new Builtin(true, "value1", new byte[]{}, 1, 1, BigDecimal.ONE, 1L, 1.1, 1.1, Duration.ZERO, LocalDate.now(), OffsetDateTime.now(), null, null, null, new HashMap<>(), null);
         model.getNullableFloatDict().put("key", null);
         String json = BinaryData.fromObject(model).toString();
     }
 
+    @Test
+    public void testSerializationForModelMapProperty() {
+        InnerModel model = new InnerModel("a");
+        Map<String, InnerModel> map = new HashMap<>();
+        map.put("b", null);
+        model.setChildren(map);
+        String json = BinaryData.fromObject(model).toString();
+    }
 }
