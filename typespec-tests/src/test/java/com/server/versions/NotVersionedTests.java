feb7fe6ea455aaf60621fe3a828b44fa17a0bae7
Weidong Xu <weidxu@microsoft.com>
2024-01-02 14:17:34 +0800

    test for version

---
+++ typespec-tests/src/test/java/com/server/versions/NotVersionedTests.java
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.server.versions;
+
+import com.server.versions.notversioned.NotVersionedClient;
+import com.server.versions.notversioned.NotVersionedClientBuilder;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+public class NotVersionedTests {
+
+    private final NotVersionedClient client = new NotVersionedClientBuilder()
+            .endpoint("http://localhost:3000")
+            .buildClient();
+
+    @Test
+    public void test() {
+        client.withoutApiVersion();
+
+        client.withPathApiVersion();
+
+        client.withQueryApiVersion();
+    }
+}
