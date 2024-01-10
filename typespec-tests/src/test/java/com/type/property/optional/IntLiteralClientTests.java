83fb7995f02de647f01c730bc738dc689f5d257e
Weidong Xu <weidxu@microsoft.com>
2024-01-10 16:00:49 +0800

    update test

--- typespec-tests/src/test/java/com/type/property/optional/IntLiteralClientTests.java
+++ typespec-tests/src/test/java/com/type/property/optional/IntLiteralClientTests.java
@@ -4,7 +4,7 @@
 package com.type.property.optional;
 
 import com.type.property.optional.models.IntLiteralProperty;
-import com.type.property.optional.models.IntLiteralProperty1;
+import com.type.property.optional.models.IntLiteralPropertyProperty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +14,7 @@ public class IntLiteralClientTests {
     @Test
     public void getAll() {
         IntLiteralProperty intLiteralProperty = client.getAll();
-        Assertions.assertEquals(IntLiteralProperty1.ONE, intLiteralProperty.getProperty());
+        Assertions.assertEquals(IntLiteralPropertyProperty.ONE, intLiteralProperty.getProperty());
     }
 
     @Test
@@ -26,7 +26,7 @@ public class IntLiteralClientTests {
     @Test
     public void putAll() {
         IntLiteralProperty intLiteralProperty = new IntLiteralProperty();
-        intLiteralProperty.setProperty(IntLiteralProperty1.ONE);
+        intLiteralProperty.setProperty(IntLiteralPropertyProperty.ONE);
         client.putAll(intLiteralProperty);
     }
 
