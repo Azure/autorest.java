55218241e361c42e9c2646bf17339f5d98d59bf8
actions-user <action@github.com>
2024-02-19 06:58:03 +0000

    re-generate test code

--- typespec-tests/src/main/java/com/cadl/armresourceprovider/models/TopLevelArmResourceProperties.java
+++ typespec-tests/src/main/java/com/cadl/armresourceprovider/models/TopLevelArmResourceProperties.java
@@ -4,20 +4,40 @@
 
 package com.cadl.armresourceprovider.models;
 
-import com.azure.core.annotation.Immutable;
+import com.azure.core.annotation.Fluent;
+import com.azure.core.util.logging.ClientLogger;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 /**
  * Top Level Arm Resource Properties.
  */
-@Immutable
+@Fluent
 public final class TopLevelArmResourceProperties {
     /*
      * Configuration Endpoints.
      */
     @JsonProperty(value = "configurationEndpoints", access = JsonProperty.Access.WRITE_ONLY)
     private List<String> configurationEndpoints;
+
+    /*
+     * The userName property.
+     */
+    @JsonProperty(value = "userName", required = true)
+    private String username;
+
+    /*
+     * The ipRules property.
+     */
+    @JsonProperty(value = "ipRules", required = true)
+    private List<IpRules> ipRules;
+
+    /*
+     * The startTimeStamp property.
+     */
+    @JsonProperty(value = "startTimeStamp", required = true)
+    private OffsetDateTime startTimestamp;
 
     /*
      * The status of the last operation.
@@ -41,6 +61,66 @@ public final class TopLevelArmResourceProperties {
     }
 
     /**
+     * Get the username property: The userName property.
+     * 
+     * @return the username value.
+     */
+    public String username() {
+        return this.username;
+    }
+
+    /**
+     * Set the username property: The userName property.
+     * 
+     * @param username the username value to set.
+     * @return the TopLevelArmResourceProperties object itself.
+     */
+    public TopLevelArmResourceProperties withUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    /**
+     * Get the ipRules property: The ipRules property.
+     * 
+     * @return the ipRules value.
+     */
+    public List<IpRules> ipRules() {
+        return this.ipRules;
+    }
+
+    /**
+     * Set the ipRules property: The ipRules property.
+     * 
+     * @param ipRules the ipRules value to set.
+     * @return the TopLevelArmResourceProperties object itself.
+     */
+    public TopLevelArmResourceProperties withIpRules(List<IpRules> ipRules) {
+        this.ipRules = ipRules;
+        return this;
+    }
+
+    /**
+     * Get the startTimestamp property: The startTimeStamp property.
+     * 
+     * @return the startTimestamp value.
+     */
+    public OffsetDateTime startTimestamp() {
+        return this.startTimestamp;
+    }
+
+    /**
+     * Set the startTimestamp property: The startTimeStamp property.
+     * 
+     * @param startTimestamp the startTimestamp value to set.
+     * @return the TopLevelArmResourceProperties object itself.
+     */
+    public TopLevelArmResourceProperties withStartTimestamp(OffsetDateTime startTimestamp) {
+        this.startTimestamp = startTimestamp;
+        return this;
+    }
+
+    /**
      * Get the provisioningState property: The status of the last operation.
      * 
      * @return the provisioningState value.
@@ -55,5 +135,21 @@ public final class TopLevelArmResourceProperties {
      * @throws IllegalArgumentException thrown if the instance is not valid.
      */
     public void validate() {
+        if (username() == null) {
+            throw LOGGER.logExceptionAsError(new IllegalArgumentException(
+                "Missing required property username in model TopLevelArmResourceProperties"));
+        }
+        if (ipRules() == null) {
+            throw LOGGER.logExceptionAsError(new IllegalArgumentException(
+                "Missing required property ipRules in model TopLevelArmResourceProperties"));
+        } else {
+            ipRules().forEach(e -> e.validate());
+        }
+        if (startTimestamp() == null) {
+            throw LOGGER.logExceptionAsError(new IllegalArgumentException(
+                "Missing required property startTimestamp in model TopLevelArmResourceProperties"));
+        }
     }
+
+    private static final ClientLogger LOGGER = new ClientLogger(TopLevelArmResourceProperties.class);
 }
