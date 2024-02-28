789deb2b4874b6cff6eb2e92d6b6f0822fcfebff
Weidong Xu <weidxu@microsoft.com>
2024-02-28 14:14:41 +0800

    script

---
+++ PreparePatchRelease.ps1
@@ -0,0 +1,4 @@
+git checkout main -- typespec-extension/changelog.md
+git checkout main -- typespec-extension/package-lock.json
+git checkout main -- typespec-extension/package.json
+git checkout main -- typespec-tests/package.json
