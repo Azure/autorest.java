dc4d48392a9775f2459be4d2c3e4727f0d13c61c
Haoling Dong <haolingdong@microsoft.com>
2024-03-26 15:12:50 +0800

    apply cadl-ranch

--- typespec-tests/Apply-cadl-ranch.ps1
+++ typespec-tests/Apply-cadl-ranch.ps1
@@ -1,7 +1,9 @@
 Copy-Item -Path node_modules/@azure-tools/cadl-ranch-specs/http -Destination ./ -Recurse -Force
 
-tsp compile ./http/type/model/inheritance/nested-discriminator/main.tsp
+tsp compile ./http/type/model/inheritance/single-discriminator/main.tsp
 
-Remove-Item -Path ./src/main/java/com/type/model/inheritance/nesteddiscriminator/ -Recurse -Force
+Remove-Item -Path ./src/main/java/com/type/model/inheritance/singlediscriminator/ -Recurse -Force
 
-Copy-Item -Path ./tsp-output/src/main/java/com/type/model/inheritance/nesteddiscriminator/ -Destination ./src/main/java/com/type/model/inheritance/nesteddiscriminator/ -Recurse -Force -Exclude @("ReadmeSamples.java", "module-info.java")
+Copy-Item -Path ./tsp-output/src/main/java/com/type/model/inheritance/singlediscriminator/ -Destination ./src/main/java/com/type/model/inheritance/singlediscriminator/ -Recurse -Force -Exclude @("ReadmeSamples.java", "module-info.java")
+
+Remove-Item ./http -Recurse -Force
