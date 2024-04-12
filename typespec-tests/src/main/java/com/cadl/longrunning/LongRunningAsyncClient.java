651ca2e20f3393e5caf22b79ca23b8b4b9a1c93b
Haoling Dong <haolingdong@microsoft.com>
2024-04-12 15:02:11 +0800

    generate long running

--- typespec-tests/src/main/java/com/cadl/longrunning/LongRunningAsyncClient.java
+++ typespec-tests/src/main/java/com/cadl/longrunning/LongRunningAsyncClient.java
@@ -88,7 +88,7 @@ public final class LongRunningAsyncClient {
      * }
      * }</pre>
      * 
-     * @param id Universally Unique Identifier.
+     * @param id A sequence of textual characters.
      * @param requestOptions The options to configure the HTTP request before HTTP client sends it.
      * @throws HttpResponseException thrown if the request is rejected by server.
      * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
@@ -180,7 +180,7 @@ public final class LongRunningAsyncClient {
     /**
      * A remote procedure call (RPC) operation.
      * 
-     * @param id Universally Unique Identifier.
+     * @param id A sequence of textual characters.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws HttpResponseException thrown if the request is rejected by server.
      * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
