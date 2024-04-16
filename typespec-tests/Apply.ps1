0d03ee08a4e49070b4eef36a5260623ea99e6cb1
Haoling Dong <haolingdong@microsoft.com>
2024-04-16 17:16:57 +0800

    clean up

--- typespec-tests/Apply.ps1
+++
@@ -1,9 +0,0 @@
-
-Remove-Item ./tsp-output -Recurse -Force
-
-tsp compile tsp/naming.tsp
-
-Remove-Item -Path ./src/main/java/com/cadl/naming -Recurse -Force -ErrorAction SilentlyContinue
-
-Copy-Item -Path ./tsp-output/src/main/java/com/cadl/naming -Destination ./src/main/java/com/cadl/ -Recurse -Force -Exclude @("ReadmeSamples.java", "module-info.java")
-
