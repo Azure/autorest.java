78d173ddfa269c25d03e7bc8c11cbc43402bc7d6
Hong Li(MSFT) <74638143+v-hongli1@users.noreply.github.com>
2024-05-07 17:03:04 +0800

    The PR for tsp, add test cases for Encode/Parameters/Versioning/Type (#2746)

---
+++ typespec-tests/src/test/java/com/type/model/flatten/FlattenClientTests.java
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.type.model.flatten;
+
+import com.type.model.flatten.models.ChildFlattenModel;
+import com.type.model.flatten.models.ChildModel;
+import com.type.model.flatten.models.FlattenModel;
+import com.type.model.flatten.models.NestedFlattenModel;
+import org.junit.jupiter.api.Test;
+
+public class FlattenClientTests {
+    private final FlattenClient flattenClient = new FlattenClientBuilder().buildClient();
+
+    @Test
+    public void testFlattenModel() {
+        // flatten is not enabled for data-plane
+        flattenClient.putFlattenModel(new FlattenModel("foo", new ChildModel("bar", 10)));
+        flattenClient.putNestedFlattenModel(new NestedFlattenModel("foo", new ChildFlattenModel("bar", new ChildModel("test",10))));
+    }
+}
