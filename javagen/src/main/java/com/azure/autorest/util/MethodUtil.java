aa233ca903f6441abd1da3906e7a6bc6e6be28f0
Haoling Dong <haolingdong@microsoft.com>
2024-05-21 14:58:19 +0800

    remove the fallback to summary and description logics, also only use dummy description when both summary and description are empty

--- javagen/src/main/java/com/azure/autorest/util/MethodUtil.java
+++ javagen/src/main/java/com/azure/autorest/util/MethodUtil.java
@@ -98,22 +98,17 @@ public class MethodUtil {
         if (parameter.getLanguage() != null) {
             description = parameter.getLanguage().getDefault().getDescription();
         }
-        // fallback to parameter schema description
-        if (description == null || description.isEmpty()) {
-            if (parameter.getSchema() != null && parameter.getSchema().getLanguage() != null) {
-                description = parameter.getSchema().getLanguage().getDefault().getDescription();
-            }
-        }
-        // fallback to dummy description
-        if (description == null || description.isEmpty()) {
-            description = "The " + name + " parameter";
-        }
+
         // add allowed enum values
         if (isProtocolMethod && parameter.getProtocol().getHttp().getIn() != RequestParameterLocation.BODY) {
             description = MethodUtil.appendAllowedEnumValuesForEnumType(parameter, description);
         }
 
-        return SchemaUtil.mergeSummaryWithDescription(summary, description);
+        String javadocDescription = SchemaUtil.mergeSummaryWithDescription(summary, description);
+        if (CoreUtils.isNullOrEmpty(javadocDescription)) { // fallback to dummy description only when both summary and description are empty
+            javadocDescription = "The " + name + " parameter";
+        }
+        return javadocDescription;
     }
 
     /**
