aa2cfd82251f0d09bddbecbee56055c025bf92fe
hongli750210 <v-hongli1@microsoft.com>
2024-07-17 16:20:52 +0800

    Add test cases for Type/Property/Optional/Plaintime

---
+++ typespec-tests/src/test/java/com/type/property/optional/PlaintimeClientTests.java
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.type.property.optional;
+
+import com.type.property.optional.models.PlainTimeProperty;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PlaintimeClientTests {
+    private final PlaintimeClient client = new OptionalClientBuilder().buildPlaintimeClient();
+
+    @Test
+    public void getAll() {
+        Assertions.assertEquals("13:06:12", client.getAll().getProperty());
+    }
+
+    @Test
+    public void getDefault() {
+        Assertions.assertNotNull(client.getDefault());
+    }
+
+    @Test
+    public void putAll() {
+        client.putAll(new PlainTimeProperty().setProperty("13:06:12"));
+    }
+
+    @Test
+    public void putDefault() {
+        client.putDefault(new PlainTimeProperty());
+    }
+}
