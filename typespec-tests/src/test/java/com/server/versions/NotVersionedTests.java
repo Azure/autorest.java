e8a8d3b3b2bdd3309274434eaa947b416f4a87c1
Haoling Dong <haolingdong@microsoft.com>
2024-08-19 11:13:24 +0800

    fix NotVersionedTests

--- typespec-tests/src/test/java/com/server/versions/NotVersionedTests.java
+++ typespec-tests/src/test/java/com/server/versions/NotVersionedTests.java
@@ -17,8 +17,8 @@ public class NotVersionedTests {
     public void test() {
         client.withoutApiVersion();
 
-        client.withPathApiVersion("v1.0");
+        client.withPathApiVersion();
 
-        client.withQueryApiVersion("v1.0");
+        client.withQueryApiVersion();
     }
 }
