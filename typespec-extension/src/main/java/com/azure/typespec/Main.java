3005055dc11a83407b698b0f0d5be635f3ee8028
xiaofeicao <xiaofeicao@microsoft.com>
2024-08-23 14:31:57 +0800

    checkstyle

--- typespec-extension/src/main/java/com/azure/typespec/Main.java
+++ typespec-extension/src/main/java/com/azure/typespec/Main.java
@@ -39,7 +39,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
