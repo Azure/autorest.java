fc5ddcec19a88f05282fbc9f6312ea5df5269544
Weidong Xu <weidxu@microsoft.com>
2025-03-14 16:47:04 +0800

    remove some options that we do not expect dev to use

--- typespec-extension/src/options.ts
+++ typespec-extension/src/options.ts
@@ -9,32 +9,22 @@ export interface DevOptions {
   "java-temp-dir"?: string; // working directory for java codegen, e.g. transformed code-model file
 }
 
+// see EmitterOptionsDev in code-model-builder.ts for a full list of options
+// but many of them is not recommended to be used, except for backward-compatible or some corner cases
 export interface EmitterOptions {
   "namespace"?: string;
-
-  "flavor"?: string;
-
   "service-name"?: string;
-  "service-versions"?: string[];
-
-  "skip-special-headers"?: string[];
 
   "generate-samples"?: boolean;
   "generate-tests"?: boolean;
-
-  "enable-sync-stack"?: boolean;
-  "stream-style-serialization"?: boolean;
-  "use-object-for-unknown"?: boolean;
 
   "partial-update"?: boolean;
   "models-subpackage"?: string;
   "custom-types"?: string;
   "custom-types-subpackage"?: string;
   "customization-class"?: string;
-  "polling"?: any;
 
-  "group-etag-headers"?: boolean;
-
+  "skip-special-headers"?: string[];
   "enable-subclient"?: boolean;
 
   "advanced-versioning"?: boolean;
@@ -48,16 +38,9 @@ export const EmitterOptionsSchema: JSONSchemaType<EmitterOptions> = {
   type: "object",
   additionalProperties: true,
   properties: {
-    "namespace": { type: "string", nullable: true },
-
-    "flavor": { type: "string", nullable: true },
-
     // service
+    "namespace": { type: "string", nullable: true },
     "service-name": { type: "string", nullable: true },
-    "service-versions": { type: "array", items: { type: "string" }, nullable: true },
-
-    // header
-    "skip-special-headers": { type: "array", items: { type: "string" }, nullable: true },
 
     // sample and test
     "generate-samples": { type: "boolean", nullable: true, default: true },
@@ -73,16 +56,17 @@ export const EmitterOptionsSchema: JSONSchemaType<EmitterOptions> = {
     "custom-types": { type: "string", nullable: true },
     "custom-types-subpackage": { type: "string", nullable: true },
     "customization-class": { type: "string", nullable: true },
-    "polling": { type: "object", additionalProperties: true, nullable: true },
 
-    "group-etag-headers": { type: "boolean", nullable: true },
-
+    // configure
+    "skip-special-headers": { type: "array", items: { type: "string" }, nullable: true },
     "enable-subclient": { type: "boolean", nullable: true, default: false },
 
-    "advanced-versioning": { type: "boolean", nullable: true, default: false },
+    // versioning
     "api-version": { type: "string", nullable: true },
+    "advanced-versioning": { type: "boolean", nullable: true, default: false },
     "service-version-exclude-preview": { type: "boolean", nullable: true, default: false },
 
+    // dev options
     "dev-options": { type: "object", additionalProperties: true, nullable: true },
   },
   required: [],
