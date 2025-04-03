e86f826eae8c74abb5b9a48b054bdfff683dac45
Weidong Xu <weidxu@microsoft.com>
2025-04-03 12:22:27 +0800

    filter modules

--- eng/sdk/sync_sdk.py
+++ eng/sdk/sync_sdk.py
@@ -114,7 +114,7 @@ def update_sdks():
             continue
 
         commit_id = "324bec9812c45197ad9d65174af0f3ee87325ce4"
-        if commit_id:
+        if commit_id and artifact in ['azure-ai-translation-text', 'azure-ai-translation-document', 'azure-analytics-purview-datamap', 'azure-ai-openai-assistants', 'azure-ai-openai', 'azure-resourcemanager-databasewatcher']:
             with open(tsp_location_file, "r", encoding="utf-8") as f_in:
                 lines = f_in.readlines()
             lines_out = []
